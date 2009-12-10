6374684802ed0dc04c3143ef67d4b519d2d4d553
matthewhorridge <matthewhorridge@02e1a946-7720-0410-8c3b-a7e05feb851e>
2009-12-10 16:17:18 +0000

    Test case for 2543981
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1382 02e1a946-7720-0410-8c3b-a7e05feb851e

---
+++ apibinding/src/test/java/org/semanticweb/owlapi/api/test/IRISubstringTestCase.java
@@ -0,0 +1,52 @@
+package org.semanticweb.owlapi.api.test;
+
+import org.semanticweb.owlapi.model.*;
+import org.coode.xml.XMLWriterPreferences;
+
+import java.util.Set;
+import java.util.HashSet;
+/*
+ * Copyright (C) 2009, University of Manchester
+ *
+ * Modifications to the initial code base are copyright of their
+ * respective authors, or their employers as appropriate.  Authorship
+ * of the modifications may be determined from the ChangeLog placed at
+ * the end of this file.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Author: Matthew Horridge<br>
+ * The University of Manchester<br>
+ * Information Management Group<br>
+ * Date: 10-Dec-2009
+ */
+public class IRISubstringTestCase extends AbstractAxiomsRoundTrippingTestCase {
+
+    protected Set<? extends OWLAxiom> createAxioms() {
+        XMLWriterPreferences.getInstance().setUseNamespaceEntities(true);
+        IRI iriA = IRI.create("http://owlapi.sourceforge.net/properties#propA");
+        IRI iriB = IRI.create("http://owlapi.sourceforge.net/properties2#propB");
+        Set<OWLAxiom> axioms = new HashSet<OWLAxiom>();
+        OWLClass clsA = getOWLClass("A");
+        OWLAnnotationProperty propA = getFactory().getOWLAnnotationProperty(iriA);
+        OWLAnnotationProperty propB = getFactory().getOWLAnnotationProperty(iriB);
+        axioms.add(getFactory().getOWLDeclarationAxiom(clsA));
+        axioms.add(getFactory().getOWLAnnotationAssertionAxiom(propA, clsA.getIRI(), getFactory().getOWLStringLiteral("value1")));
+        axioms.add(getFactory().getOWLAnnotationAssertionAxiom(propB, clsA.getIRI(), getFactory().getOWLStringLiteral("value2")));
+        return axioms;
+    }
+}
