d4838bf0e14b7a968fbfe110403ddd43fcae8d30
matthewhorridge <matthewhorridge@02e1a946-7720-0410-8c3b-a7e05feb851e>
2009-12-17 13:25:44 +0000

    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1399 02e1a946-7720-0410-8c3b-a7e05feb851e

--- api/src/main/java/org/semanticweb/owlapi/io/OWLOntologyInputSource.java
+++ api/src/main/java/org/semanticweb/owlapi/io/OWLOntologyInputSource.java
@@ -22,8 +22,7 @@ import java.net.URI;
  * Lesser General Public License for more details.
 
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * License along with this library; if not, If not, see <http://www.gnu.org/licenses/>.
  */
 
 
