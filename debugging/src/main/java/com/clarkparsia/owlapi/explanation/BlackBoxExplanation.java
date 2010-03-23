owlcs/owlapi

commit 324f08d0295e41f824482c637ab47149db53e605

git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1367 02e1a946-7720-0410-8c3b-a7e05feb851e

debugging/src/main/java/com/clarkparsia/owlapi/explanation/BlackBoxExplanation.java
@@ -1,9 +1,11 @@
 package com.clarkparsia.owlapi.explanation;
 
-import org.semanticweb.owlapi.inference.OWLClassReasoner;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.util.OWLAxiomVisitorAdapter;
 import org.semanticweb.owlapi.util.OWLEntityCollector;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.apibinding.OWLManager;
 
 import java.net.URI;
 import java.util.*;
@@ -55,8 +57,7 @@ import com.clarkparsia.owlapi.explanation.util.OntologyUtils;
  */
 public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implements SingleExplanationGenerator {
 
-    public static final Logger log = Logger
-            .getLogger(BlackBoxExplanation.class.getName());
+    public static final Logger log = Logger.getLogger(BlackBoxExplanation.class.getName());
 
     private OWLOntology debuggingOntology;
 
@@ -86,10 +87,11 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
 
     private boolean performRepeatedFastPruning = false;
 
+    private OWLOntologyManager owlOntologyManager;
 
-    public BlackBoxExplanation(OWLOntologyManager manager) {
-        super(manager);
-
+    public BlackBoxExplanation(OWLOntology ontology, OWLReasonerFactory reasonerFactory, OWLReasoner reasoner) {
+        super(ontology, reasonerFactory, reasoner);
+        owlOntologyManager = OWLManager.createOWLOntologyManager();
         debuggingAxioms = new LinkedHashSet<OWLAxiom>();
         objectsExpandedWithDefiningAxioms = new HashSet<OWLEntity>();
         objectsExpandedWithReferencingAxioms = new HashSet<OWLEntity>();
@@ -100,13 +102,8 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
 
 
     public void dispose() {
-        try {
-            reset();
-            reasoner.dispose();
-        }
-        catch (OWLException e) {
-            throw new OWLRuntimeException(e);
-        }
+        reset();
+        getReasoner().dispose();
     }
 
 
@@ -127,42 +124,22 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
 
     public Set<OWLAxiom> getExplanation(OWLClassExpression unsatClass) {
 
-        if (!definitionTracker.isDefined(unsatClass))
+        if (!getDefinitionTracker().isDefined(unsatClass))
             return Collections.emptySet();
 
         try {
-            boolean firstExplanation = isFirstExplanation();
-
-            // save the reasoner so we won't have to reload anything
-            OWLClassReasoner savedReasoner = reasoner;
-
-            if (!firstExplanation) {
-                reasoner = getAltReasoner();
-                reasoner.clearOntologies();
-                reasoner.loadOntologies(getOntologies());
-            }
-
             satTestCount++;
 
-            if (reasoner.isSatisfiable(unsatClass)) {
-                reasoner = savedReasoner;
+            if (isFirstExplanation()) {
+                getReasoner().isSatisfiable(unsatClass);
                 return Collections.emptySet();
             }
-
-            if (firstExplanation)
-                reasoner = getAltReasoner();
-
             reset();
-
             expandUntilUnsatisfiable(unsatClass);
-
             pruneUntilMinimal(unsatClass);
-
             removeDeclarations();
-
             ontologyCounter = 0;
 
-            reasoner = savedReasoner;
             return new HashSet<OWLAxiom>(debuggingAxioms);
         }
         catch (OWLException e) {
@@ -255,20 +232,22 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
      */
     private int expandWithDefiningAxioms(OWLEntity obj, int limit) {
         Set<OWLAxiom> expansionAxioms = new HashSet<OWLAxiom>();
-        for (OWLOntology ont : getOntologies()) {
+        for (OWLOntology ont : getOntology().getImportsClosure()) {
             boolean referenceFound = false;
             if (obj instanceof OWLClass) {
                 referenceFound = expansionAxioms.addAll(ont.getAxioms((OWLClass) obj));
-            } else if (obj instanceof OWLObjectProperty) {
+            }
+            else if (obj instanceof OWLObjectProperty) {
                 referenceFound = expansionAxioms.addAll(ont.getAxioms((OWLObjectProperty) obj));
-            } else if (obj instanceof OWLDataProperty) {
+            }
+            else if (obj instanceof OWLDataProperty) {
                 referenceFound = expansionAxioms.addAll(ont.getAxioms((OWLDataProperty) obj));
-            } else if (obj instanceof OWLIndividual) {
+            }
+            else if (obj instanceof OWLIndividual) {
                 referenceFound = expansionAxioms.addAll(ont.getAxioms((OWLIndividual) obj));
             }
             if (!referenceFound)
-                expansionAxioms.add(owlOntologyManager.getOWLDataFactory().getOWLDeclarationAxiom(
-                        obj));
+                expansionAxioms.add(owlOntologyManager.getOWLDataFactory().getOWLDeclarationAxiom(obj));
         }
         expansionAxioms.removeAll(debuggingAxioms);
         return addMax(expansionAxioms, debuggingAxioms, limit);
@@ -283,7 +262,7 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
         Set<OWLAxiom> expansionAxioms = new HashSet<OWLAxiom>();
         // First expand by getting the defining axioms - if this doesn't
         // return any axioms, then get the axioms that reference the entity
-        for (OWLOntology ont : getOntologies()) {
+        for (OWLOntology ont : getOntology().getImportsClosure()) {
             expansionAxioms.addAll(ont.getReferencingAxioms(obj));
         }
         expansionAxioms.removeAll(debuggingAxioms);
@@ -296,9 +275,9 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
      * specified limit. Annotation axioms are stripped out
      *
      * @param source The source set. Objects from this set will be added to the
-     *               destination set
-     * @param dest   The destination set. Objects will be added to this set
-     * @param limit  The maximum number of objects to be added.
+     * destination set
+     * @param dest The destination set. Objects will be added to this set
+     * @param limit The maximum number of objects to be added.
      * @return The number of objects that were actually added.
      */
     private static <N extends OWLAxiom> int addMax(Set<N> source, Set<N> dest, int limit) {
@@ -415,18 +394,14 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
         createDebuggingOntology();
         ontologyCounter++;
 
-        if (reasoner == null) {
-            reasoner = getAltReasoner();
-        }
-        reasoner.clearOntologies();
-        reasoner.loadOntologies(Collections.singleton(debuggingOntology));
+        OWLReasoner reasoner = getReasonerFactory().createReasoner(getOntology());
 
         if (OntologyUtils.containsUnreferencedEntity(debuggingOntology, unsatClass))
             return true;
-
         satTestCount++;
-
-        return reasoner.isSatisfiable(unsatClass);
+        boolean sat = reasoner.isSatisfiable(unsatClass);
+        reasoner.dispose();
+        return sat;
     }
 
 
@@ -460,7 +435,8 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
 
         if (!unsatClass.isAnonymous()) {
             expandWithDefiningAxioms((OWLClass) unsatClass, expansionLimit);
-        } else {
+        }
+        else {
             OWLClass owlThing = owlOntologyManager.getOWLDataFactory().getOWLThing();
             OWLSubClassOfAxiom axiom = owlOntologyManager.getOWLDataFactory().getOWLSubClassOfAxiom(unsatClass, owlThing);
             debuggingAxioms.add(axiom);
@@ -541,13 +517,13 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
                 log.fine("... end of fast pruning. Axioms remaining: " + debuggingAxioms.size());
                 log.fine("Performed " + satTestCount + " satisfiability tests during fast pruning");
             }
-        } else {
+        }
+        else {
             fastPruningWindowSize = DEFAULT_FAST_PRUNING_WINDOW_SIZE;
             performFastPruning(unsatClass);
             if (log.isLoggable(Level.FINE)) {
                 log.info("... end of fast pruning. Axioms remaining: " + debuggingAxioms.size());
-                log
-                        .info("Performed " + satTestCount + " satisfiability tests during fast pruning");
+                log.info("Performed " + satTestCount + " satisfiability tests during fast pruning");
             }
         }
 

api/src/main/java/org/semanticweb/owlapi/reasoner/OWLReasoner.java
@@ -313,6 +313,13 @@ public interface OWLReasoner {
      */
     boolean isSatisfiable(OWLClassExpression classExpression) throws ReasonerInterruptedException, TimeOutException, ClassExpressionNotInProfileException, UndeclaredEntitiesException, InconsistentOntologyException;
 
+    /**
+     * A convenience method that obtains the classes in the signature of the root ontology that are unsatisfiable.
+     * @return
+     * @throws ReasonerInterruptedException
+     * @throws TimeOutException
+     */
+    Set<OWLClass> getUnsatisfiableClasses() throws ReasonerInterruptedException, TimeOutException;
 
     /**
      * A convenience method that determines if the specified axiom is entailed by the set of reasoner axioms.

api/src/main/java/org/semanticweb/owlapi/reasoner/OWLReasonerFactory.java
@@ -43,6 +43,12 @@ import java.util.Set;
 public interface OWLReasonerFactory {
 
     /**
+     * Gets the name of the reasoner created by this factory.
+     * @return A string that represents the name of the reasoner created by this factory.
+     */
+    String getReasonerName();
+
+    /**
      * Creates an OWLReasoner that reasons over the imports closure of the specified ontology.  The reasoner will obtain
      * the imports closure from the specified ontology manager.  The reasoner will listen for ontology changes to the ontologies
      * that it is reasoning over and will always answer queries with respect to the changed ontologies (see {@link org.semanticweb.owlapi.reasoner.OWLReasoner}

api/src/main/java/org/semanticweb/owlapi/reasoner/UnsupportedEntailmentTypeException.java
@@ -1,6 +1,7 @@
 package org.semanticweb.owlapi.reasoner;
 
 import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLRuntimeException;
 /*
  * Copyright (C) 2009, University of Manchester
  *
@@ -30,7 +31,7 @@ import org.semanticweb.owlapi.model.OWLAxiom;
  * Information Management Group<br>
  * Date: 18-Mar-2009
  */
-public class UnsupportedEntailmentTypeException extends OWLReasonerException {
+public class UnsupportedEntailmentTypeException extends OWLRuntimeException {
 
     private OWLAxiom axiom;
 

api/src/main/java/org/semanticweb/owlapi/util/InferredAxiomGenerator.java
@@ -1,9 +1,8 @@
 package org.semanticweb.owlapi.util;
 
-import org.semanticweb.owlapi.inference.OWLReasoner;
-import org.semanticweb.owlapi.inference.OWLReasonerException;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 import java.util.Set;
 /*
@@ -44,7 +43,7 @@ import java.util.Set;
  */
 public interface InferredAxiomGenerator<A extends OWLAxiom> {
 
-    Set<A> createAxioms(OWLOntologyManager manager, OWLReasoner reasoner) throws OWLReasonerException;
+    Set<A> createAxioms(OWLOntologyManager manager, OWLReasoner reasoner);
 
     String getLabel();
 }

api/src/main/java/org/semanticweb/owlapi/util/InferredAxiomGeneratorException.java
@@ -1,67 +1,0 @@
-package org.semanticweb.owlapi.util;
-
-import org.semanticweb.owlapi.inference.OWLReasonerException;
-import org.semanticweb.owlapi.model.OWLException;
-/*
- * Copyright (C) 2007, University of Manchester
- *
- * Modifications to the initial code base are copyright of their
- * respective authors, or their employers as appropriate.  Authorship
- * of the modifications may be determined from the ChangeLog placed at
- * the end of this file.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
-
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-
-/**
- * Author: Matthew Horridge<br>
- * The University Of Manchester<br>
- * Bio-Health Informatics Group<br>
- * Date: 27-Jul-2007<br><br>
- *
- * Represents an error where an inferred axiom generator could not generate
- * inferred axioms because the reasoner doesn't support the queries that are
- * required to generate the axioms.
- */
-public class InferredAxiomGeneratorException extends OWLException {
-
-    private InferredAxiomGenerator generator;
-
-    private OWLReasonerException reasonerException;
-
-
-    public InferredAxiomGeneratorException(InferredAxiomGenerator generator,
-                                           OWLReasonerException reasonerException) {
-        super(getMessage(generator), reasonerException);
-        this.generator = generator;
-        this.reasonerException = reasonerException;
-    }
-
-    private static String getMessage(InferredAxiomGenerator gen) {
-        return "Could not generate inferred axioms for " + gen +
-                " because there was a problem when querying the reasoner for these axioms";
-    }
-
-
-    public InferredAxiomGenerator getGenerator() {
-        return generator;
-    }
-
-
-    public OWLReasonerException getReasonerException() {
-        return reasonerException;
-    }
-}

api/src/main/java/org/semanticweb/owlapi/util/InferredClassAssertionAxiomGenerator.java
@@ -1,12 +1,10 @@
 package org.semanticweb.owlapi.util;
 
-import org.semanticweb.owlapi.inference.OWLReasoner;
-import org.semanticweb.owlapi.inference.OWLReasonerAdapter;
-import org.semanticweb.owlapi.inference.OWLReasonerException;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 import java.util.Set;
 /*
@@ -44,9 +42,8 @@ import java.util.Set;
 public class InferredClassAssertionAxiomGenerator extends InferredIndividualAxiomGenerator<OWLClassAssertionAxiom> {
 
 
-    protected void addAxioms(OWLNamedIndividual entity, OWLReasoner reasoner, OWLDataFactory dataFactory, Set<OWLClassAssertionAxiom> result) throws
-            OWLReasonerException {
-        for (OWLClass type : OWLReasonerAdapter.flattenSetOfSets(reasoner.getTypes(entity, false))) {
+    protected void addAxioms(OWLNamedIndividual entity, OWLReasoner reasoner, OWLDataFactory dataFactory, Set<OWLClassAssertionAxiom> result) {
+        for (OWLClass type : reasoner.getTypes(entity, false).getFlattened()) {
             result.add(dataFactory.getOWLClassAssertionAxiom(type, entity));
         }
     }

api/src/main/java/org/semanticweb/owlapi/util/InferredDataPropertyCharacteristicAxiomGenerator.java
@@ -1,10 +1,10 @@
 package org.semanticweb.owlapi.util;
 
-import org.semanticweb.owlapi.inference.OWLReasoner;
-import org.semanticweb.owlapi.inference.OWLReasonerException;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLDataProperty;
 import org.semanticweb.owlapi.model.OWLDataPropertyCharacteristicAxiom;
+import org.semanticweb.owlapi.model.OWLFunctionalDataPropertyAxiom;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 import java.util.Set;
 /*
@@ -42,10 +42,10 @@ import java.util.Set;
 public class InferredDataPropertyCharacteristicAxiomGenerator extends InferredDataPropertyAxiomGenerator<OWLDataPropertyCharacteristicAxiom> {
 
 
-    protected void addAxioms(OWLDataProperty entity, OWLReasoner reasoner, OWLDataFactory dataFactory, Set<OWLDataPropertyCharacteristicAxiom> result) throws
-            OWLReasonerException {
-        if (reasoner.isFunctional(entity)) {
-            result.add(dataFactory.getOWLFunctionalDataPropertyAxiom(entity));
+    protected void addAxioms(OWLDataProperty entity, OWLReasoner reasoner, OWLDataFactory dataFactory, Set<OWLDataPropertyCharacteristicAxiom> result) {
+        OWLFunctionalDataPropertyAxiom axiom = dataFactory.getOWLFunctionalDataPropertyAxiom(entity);
+        if (reasoner.isEntailed(axiom) && reasoner.isEntailed(axiom)) {
+            result.add(axiom);
         }
     }
 

commit d03f09eacd2f625ccb2304ce02c4a486c2e1c85f

Fix the bugs introduced in r1367 that cause BlackBoxExplanation always return empty explanations
git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1447 02e1a946-7720-0410-8c3b-a7e05feb851e

debugging/src/main/java/com/clarkparsia/owlapi/explanation/BlackBoxExplanation.java
@@ -131,8 +131,8 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
             satTestCount++;
 
             if (isFirstExplanation()) {
-                getReasoner().isSatisfiable(unsatClass);
-                return Collections.emptySet();
+                if (getReasoner().isSatisfiable(unsatClass))
+                	return Collections.emptySet();
             }
             reset();
             expandUntilUnsatisfiable(unsatClass);
@@ -394,7 +394,7 @@ public class BlackBoxExplanation extends SingleExplanationGeneratorImpl implemen
         createDebuggingOntology();
         ontologyCounter++;
 
-        OWLReasoner reasoner = getReasonerFactory().createNonBufferingReasoner(getOntology());
+        OWLReasoner reasoner = getReasonerFactory().createNonBufferingReasoner(debuggingOntology);
 
         if (OntologyUtils.containsUnreferencedEntity(debuggingOntology, unsatClass))
             return true;