78dd3bdec76c2a1d21ca4de43db910bb9530c49c
matthewhorridge <matthewhorridge@02e1a946-7720-0410-8c3b-a7e05feb851e>
2010-07-01 16:40:00 +0000

    Test case for annotation accessors
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1492 02e1a946-7720-0410-8c3b-a7e05feb851e

---
+++ apibinding/src/test/java/org/semanticweb/owlapi/api/test/AnnotationAccessorsTestCase.java
@@ -0,0 +1,71 @@
+package org.semanticweb.owlapi.api.test;
+
+import org.semanticweb.owlapi.model.*;
+
+/**
+ * Author: Matthew Horridge<br>
+ * The University of Manchester<br>
+ * Bio-Health Informatics Group<br>
+ * Date: 01-Jul-2010
+ */
+public class AnnotationAccessorsTestCase extends AbstractOWLAPITestCase {
+
+    private static final IRI SUBJECT = IRI.create("http://owlapi.sourceforge.net/ontologies/test#X");
+
+    private OWLAnnotationAssertionAxiom createAnnotationAssertionAxiom() {
+        OWLAnnotationProperty prop = getOWLAnnotationProperty("prop");
+        OWLAnnotationValue value = getFactory().getOWLStringLiteral("value");
+        return getFactory().getOWLAnnotationAssertionAxiom(prop, SUBJECT, value);
+    }
+
+
+    public void testClassAccessor() {
+        OWLOntology ont = getOWLOntology("ontology");
+        OWLAnnotationAssertionAxiom ax = createAnnotationAssertionAxiom();
+        getManager().addAxiom(ont, ax);
+        assertTrue(ont.getAnnotationAssertionAxioms(SUBJECT).contains(ax));
+        OWLClass cls = getFactory().getOWLClass(SUBJECT);
+        assertTrue(cls.getAnnotationAssertionAxioms(ont).contains(ax));
+        assertTrue(cls.getAnnotations(ont).contains(ax.getAnnotation()));
+    }
+    
+    public void testNamedIndividualAccessor() {
+        OWLOntology ont = getOWLOntology("ontology");
+        OWLAnnotationAssertionAxiom ax = createAnnotationAssertionAxiom();
+        getManager().addAxiom(ont, ax);
+        assertTrue(ont.getAnnotationAssertionAxioms(SUBJECT).contains(ax));
+        OWLNamedIndividual cls = getFactory().getOWLNamedIndividual(SUBJECT);
+        assertTrue(cls.getAnnotationAssertionAxioms(ont).contains(ax));
+        assertTrue(cls.getAnnotations(ont).contains(ax.getAnnotation()));
+    }
+    
+    public void testObjectPropertyAccessor() {
+        OWLOntology ont = getOWLOntology("ontology");
+        OWLAnnotationAssertionAxiom ax = createAnnotationAssertionAxiom();
+        getManager().addAxiom(ont, ax);
+        assertTrue(ont.getAnnotationAssertionAxioms(SUBJECT).contains(ax));
+        OWLObjectProperty cls = getFactory().getOWLObjectProperty(SUBJECT);
+        assertTrue(cls.getAnnotationAssertionAxioms(ont).contains(ax));
+        assertTrue(cls.getAnnotations(ont).contains(ax.getAnnotation()));
+    }
+    
+    public void testDataPropertyAccessor() {
+        OWLOntology ont = getOWLOntology("ontology");
+        OWLAnnotationAssertionAxiom ax = createAnnotationAssertionAxiom();
+        getManager().addAxiom(ont, ax);
+        assertTrue(ont.getAnnotationAssertionAxioms(SUBJECT).contains(ax));
+        OWLDataProperty cls = getFactory().getOWLDataProperty(SUBJECT);
+        assertTrue(cls.getAnnotationAssertionAxioms(ont).contains(ax));
+        assertTrue(cls.getAnnotations(ont).contains(ax.getAnnotation()));
+    }
+    
+    public void testDatatypeAccessor() {
+        OWLOntology ont = getOWLOntology("ontology");
+        OWLAnnotationAssertionAxiom ax = createAnnotationAssertionAxiom();
+        getManager().addAxiom(ont, ax);
+        assertTrue(ont.getAnnotationAssertionAxioms(SUBJECT).contains(ax));
+        OWLDatatype cls = getFactory().getOWLDatatype(SUBJECT);
+        assertTrue(cls.getAnnotationAssertionAxioms(ont).contains(ax));
+        assertTrue(cls.getAnnotations(ont).contains(ax.getAnnotation()));
+    }
+}
