898b52a210ea6b6278252d9a0fae890e87257eb3
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2010-07-08 09:58:19 +0000

    Class for constant entities such as top/bottom classes and properties, and top datatype
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1523 02e1a946-7720-0410-8c3b-a7e05feb851e

---
+++ api/src/main/java/org/semanticweb/owlapi/vocab/OWLDataFactoryVocabulary.java
@@ -0,0 +1,35 @@
+package org.semanticweb.owlapi.vocab;
+
+
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDatatype;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+
+import uk.ac.manchester.cs.owl.owlapi.alternateimpls.NonCachingOWLDataFactoryImpl;
+
+
+public class OWLDataFactoryVocabulary {
+	private static final OWLDataFactory factory = NonCachingOWLDataFactoryImpl
+			.getInstance();
+	/**
+	 * the built in owl:Thing class, which has a URI of
+	 * &lt;http://www.w3.org/2002/07/owl#Thing&gt;
+	 */
+	public static final OWLClass OWLThing = factory.getOWLThing();
+	/**
+	 * the built in owl:Nothing class, which has a URI of
+	 * &lt;http://www.w3.org/2002/07/owl#Nothing&gt;
+	 */
+	public static final OWLClass OWLNothing = factory.getOWLNothing();
+	public static final OWLObjectProperty OWLTopObjectProperty = factory
+			.getOWLTopObjectProperty();
+	public static final OWLDataProperty OWLTopDataProperty = factory
+			.getOWLTopDataProperty();
+	public static final OWLObjectProperty OWLBottomObjectProperty = factory
+			.getOWLBottomObjectProperty();
+	public static final OWLDataProperty OWLBottomDataProperty = factory
+			.getOWLBottomDataProperty();
+	public static final OWLDatatype TopDatatype=factory.getTopDatatype();
+}
