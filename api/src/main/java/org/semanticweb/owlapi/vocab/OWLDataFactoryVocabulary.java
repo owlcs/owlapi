50c6e4cfadc2f3ff9527b484d0067f169d584603
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2010-07-08 15:54:16 +0000

    Maximum decoupling possible from implementations: only one field requires changing to switch implementations
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1532 02e1a946-7720-0410-8c3b-a7e05feb851e

--- api/src/main/java/org/semanticweb/owlapi/vocab/OWLDataFactoryVocabulary.java
+++ api/src/main/java/org/semanticweb/owlapi/vocab/OWLDataFactoryVocabulary.java
@@ -1,17 +1,20 @@
 package org.semanticweb.owlapi.vocab;
 
-
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLDataProperty;
 import org.semanticweb.owlapi.model.OWLDatatype;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
 
-import uk.ac.manchester.cs.owl.owlapi.alternateimpls.NonCachingOWLDataFactoryImpl;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
 
-
+/**
+ * This class keeps hold of often used constants; if the implementation of
+ * OWLDataFactory and all acommpanying impl classes needs changing, this class
+ * must be modified (the factory field needs to refer to the new implementation)
+ */
 public class OWLDataFactoryVocabulary {
-	private static final OWLDataFactory factory = NonCachingOWLDataFactoryImpl
+	private static final OWLDataFactory factory = OWLDataFactoryImpl
 			.getInstance();
 	/**
 	 * the built in owl:Thing class, which has a URI of
@@ -25,11 +28,9 @@ public class OWLDataFactoryVocabulary {
 	public static final OWLClass OWLNothing = factory.getOWLNothing();
 	public static final OWLObjectProperty OWLTopObjectProperty = factory
 			.getOWLTopObjectProperty();
-	public static final OWLDataProperty OWLTopDataProperty = factory
-			.getOWLTopDataProperty();
+	public static final OWLDataProperty OWLTopDataProperty = factory.getOWLTopDataProperty();
 	public static final OWLObjectProperty OWLBottomObjectProperty = factory
 			.getOWLBottomObjectProperty();
-	public static final OWLDataProperty OWLBottomDataProperty = factory
-			.getOWLBottomDataProperty();
-	public static final OWLDatatype TopDatatype=factory.getTopDatatype();
+	public static final OWLDataProperty OWLBottomDataProperty = factory.getOWLBottomDataProperty();
+	public static final OWLDatatype TopDatatype = factory.getTopDatatype();
 }
