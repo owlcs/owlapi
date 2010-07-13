c5c6ba2ad6cf8d77ffcee5f9d8ef0ac6df37f773
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2010-07-13 13:46:37 +0000

    added a threadsafe set implementation and map implementation (uses ConcurrentHashMap)
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1541 02e1a946-7720-0410-8c3b-a7e05feb851e

--- api/src/main/java/org/semanticweb/owlapi/util/CollectionFactory.java
+++ api/src/main/java/org/semanticweb/owlapi/util/CollectionFactory.java
@@ -1,7 +1,9 @@
 package org.semanticweb.owlapi.util;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 /*
@@ -26,34 +28,137 @@ import java.util.Set;
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Author: Matthew Horridge<br>
  * The University Of Manchester<br>
  * Bio-Health Informatics Group<br>
- * Date: 10-Jan-2007<br><br>
+ * Date: 10-Jan-2007<br>
+ * <br>
  */
 public class CollectionFactory {
+	public static <T> Set<T> createSet() {
+		return new HashSet<T>();
+	}
 
-    public static <T>  Set<T> createSet() {
-        return new HashSet<T>();
-    }
+	public static <T> Set<T> createSet(Collection<T> c) {
+		return new HashSet<T>(c);
+	}
 
+	public static <T> Set<T> createSet(int initialCapacity) {
+		return new HashSet<T>(initialCapacity);
+	}
 
-    public static <T>  Set<T> createSet(int initialCapacity) {
-        return new HashSet<T>(initialCapacity);
-    }
+	public static <K, V> Map<K, V> createMap() {
+		return new HashMap<K, V>();
+	}
 
-    public static <K, V> Map<K, V> createMap() {
-        return new HashMap<K, V>();
-    }
+	public static <T> Set<T> createSet(T... elements) {
+		Set<T> result = createSet();
+		for (T t : elements) {
+			result.add(t);
+		}
+		return result;
+	}
+	
+	public static <T> Set<T> createSyncSet(){
+		return new SyncSet<T>();
+	}
+	
+	public static <K,V> Map<K,V> createSyncMap(){
+		return new ConcurrentHashMap<K, V>();
+	}
 
-    public static <T> Set<T> createSet(T ... elements) {
-        Set<T> result = createSet();
-        for(T t : elements) {
-            result.add(t);
-        }
-        return result;
-    }
+	/**
+	 * this class implements a Set using a ConcurrentHashMap as backing
+	 * structure; compared to the default HashSet implementation, this structure
+	 * is threadsafe without being completely synchronized, so it offers better
+	 * performances
+	 */
+	private static final class SyncSet<T> implements Set<T> {
+		private final ConcurrentHashMap<T, Set<T>> backingMap;
+
+		public SyncSet(ConcurrentHashMap<T, Set<T>> map) {
+			backingMap = map;
+		}
+
+		public SyncSet() {
+			this(new ConcurrentHashMap<T, Set<T>>());
+		}
+
+		public boolean add(T e) {
+			return backingMap.put(e, this) == null;
+		}
+
+		public boolean addAll(Collection<? extends T> c) {
+			boolean toReturn = false;
+			for (T o : c) {
+				toReturn = toReturn || add(o);
+			}
+			return toReturn;
+		}
+
+		public void clear() {
+			backingMap.clear();
+		}
+
+		public boolean contains(Object o) {
+			return backingMap.containsKey(o);
+		}
+
+		public boolean containsAll(Collection<?> c) {
+			boolean toReturn = true;
+			for (Object o : c) {
+				toReturn = toReturn && contains(o);
+				if (!toReturn) {
+					return toReturn;
+				}
+			}
+			return toReturn;
+		}
+
+		public boolean isEmpty() {
+			return backingMap.isEmpty();
+		}
+
+		public Iterator<T> iterator() {
+			return backingMap.keySet().iterator();
+		}
+
+		public int size() {
+			return backingMap.size();
+		}
+
+		public boolean remove(Object o) {
+			return backingMap.remove(o) != null;
+		}
+
+		public boolean removeAll(Collection<?> c) {
+			boolean toReturn = false;
+			for (Object o : c) {
+				toReturn = toReturn || remove(o);
+			}
+			return toReturn;
+		}
+
+		public boolean retainAll(Collection<?> c) {
+			boolean toReturn = false;
+			for (Map.Entry<T, Set<T>> e : backingMap.entrySet()) {
+				if (!c.contains(e.getKey())) {
+					toReturn = true;
+					backingMap.remove(e.getKey());
+				}
+			}
+			return toReturn;
+		}
+
+		public Object[] toArray() {
+			return backingMap.keySet().toArray();
+		}
+
+		public <T> T[] toArray(T[] a) {
+			return backingMap.keySet().toArray(a);
+		}
+	}
 }
