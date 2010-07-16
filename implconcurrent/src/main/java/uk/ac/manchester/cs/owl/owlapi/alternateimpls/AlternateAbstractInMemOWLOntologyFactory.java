35a21760806befd3b481985b38c00d04cca52823
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2010-07-16 12:02:41 +0000

    copyright notices
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1559 02e1a946-7720-0410-8c3b-a7e05feb851e

--- implconcurrent/src/main/java/uk/ac/manchester/cs/owl/owlapi/alternateimpls/AlternateAbstractInMemOWLOntologyFactory.java
+++ implconcurrent/src/main/java/uk/ac/manchester/cs/owl/owlapi/alternateimpls/AlternateAbstractInMemOWLOntologyFactory.java
@@ -1,5 +1,28 @@
 package uk.ac.manchester.cs.owl.owlapi.alternateimpls;
 
+/*
+ * Copyright (C) 2010, University of Manchester
+ *
+ * Modifications to the initial code base are copyright of their
+ * respective authors, or their employers as appropriate.  Authorship
+ * of the modifications may be determined from the ChangeLog placed at
+ * the end of this file.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 import org.semanticweb.owlapi.apibinding.configurables.ThreadSafeOWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
@@ -8,13 +31,6 @@ import org.semanticweb.owlapi.model.OWLOntologyFactory;
 import org.semanticweb.owlapi.model.OWLOntologyID;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
-
-/**
- * Author: Matthew Horridge<br>
- * The University Of Manchester<br>
- * Bio-Health Informatics Group<br>
- * Date: 15-Nov-2006<br><br>
- */
 public abstract class AlternateAbstractInMemOWLOntologyFactory implements OWLOntologyFactory {
 
     private OWLOntologyManager ontologyManager;
--- implconcurrent/src/main/java/uk/ac/manchester/cs/owl/owlapi/alternateimpls/LockingOWLOntologyManagerImpl.java
+++ implconcurrent/src/main/java/uk/ac/manchester/cs/owl/owlapi/alternateimpls/LockingOWLOntologyManagerImpl.java
@@ -1,5 +1,28 @@
 package uk.ac.manchester.cs.owl.owlapi.alternateimpls;
 
+/*
+ * Copyright (C) 2010, University of Manchester
+ *
+ * Modifications to the initial code base are copyright of their
+ * respective authors, or their employers as appropriate.  Authorship
+ * of the modifications may be determined from the ChangeLog placed at
+ * the end of this file.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -76,13 +99,6 @@ import org.semanticweb.owlapi.model.UnloadableImportException;
 import org.semanticweb.owlapi.util.CollectionFactory;
 import org.semanticweb.owlapi.util.NonMappingOntologyIRIMapper;
 
-/**
- * Author: Matthew Horridge<br>
- * The University Of Manchester<br>
- * Bio-Health Informatics Group<br>
- * Date: 27-Oct-2006<br>
- * <br>
- */
 public class LockingOWLOntologyManagerImpl implements OWLOntologyManager,
 		OWLOntologyFactory.OWLOntologyCreationHandler {
 	private static final Logger logger = Logger
