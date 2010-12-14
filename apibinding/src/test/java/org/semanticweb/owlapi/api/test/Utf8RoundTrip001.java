706ddb0ba64fbe92924756aa94c0847440672eed
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2010-12-14 17:32:13 +0000

    test for UTF-8 escape sequences (contributed by Timothy Redmond)
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1635 02e1a946-7720-0410-8c3b-a7e05feb851e

---
+++ apibinding/src/test/java/org/semanticweb/owlapi/api/test/Utf8RoundTrip001.java
@@ -0,0 +1,67 @@
+package org.semanticweb.owlapi.api.test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.OWLFunctionalSyntaxOntologyFormat;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+
+public class Utf8RoundTrip001 extends TestCase {
+	public void testUTF8roundTrip() {
+		try {
+			OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+			String onto = "Ontology(<http://protege.org/UTF8.owl>"
+					+ "Declaration(Class(<http://protege.org/UTF8.owl#A>))"
+					+ "AnnotationAssertion(<http://www.w3.org/2000/01/rdf-schema#label> <http://protege.org/UTF8.owl#A> "
+					+ "\"Chinese=處方\"^^<http://www.w3.org/2001/XMLSchema#string>))";
+			ByteArrayInputStream in = new ByteArrayInputStream(onto.getBytes());
+			ByteArrayOutputStream out=new ByteArrayOutputStream();
+			manager.saveOntology(manager.loadOntologyFromOntologyDocument(in),
+					out);
+			System.out.println("Utf8RoundTrip001.testUTF8roundTrip()\n"+out.toString());
+		} catch (Throwable t) {
+			t.printStackTrace();
+			fail();
+		}
+	}
+
+	public void testPositiveUTF8roundTrip() {
+		try {
+			String NS = "http://protege.org/UTF8.owl";
+			OWLOntologyManager manager;
+			OWLOntology ontology;
+			manager = OWLManager.createOWLOntologyManager();
+			ontology = manager.createOntology(IRI.create(NS));
+			OWLDataFactory factory = manager.getOWLDataFactory();
+			OWLClass a = factory.getOWLClass(IRI.create(NS + "#A"));
+			manager.addAxiom(ontology, factory.getOWLDeclarationAxiom(a));
+			OWLAnnotationAssertionAxiom axiom = factory
+					.getOWLAnnotationAssertionAxiom(a.getIRI(), factory
+							.getOWLAnnotation(factory.getRDFSLabel(),
+									factory.getOWLLiteral("Chinese=處方")));
+			manager.addAxiom(ontology, axiom);
+			File ontFile = File.createTempFile("OWLApi-utf8", "owl");
+			manager.saveOntology(ontology,
+					new OWLFunctionalSyntaxOntologyFormat(),
+					IRI.create(ontFile));
+			System.out.println("Saved as " + ontFile);
+			manager = OWLManager.createOWLOntologyManager();
+			ontology = manager.loadOntologyFromOntologyDocument(ontFile);
+		} catch (Throwable t) {
+			t.printStackTrace();
+			fail();
+		}
+	}
+}
