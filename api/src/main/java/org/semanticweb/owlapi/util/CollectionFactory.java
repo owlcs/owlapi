ac34d98937056e473da21f0c2c159c13b037f76e
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2010-12-14 19:42:40 +0000

    added support for smart handling of many (>10) contains/containsAll when the source collection is not a set.
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1639 02e1a946-7720-0410-8c3b-a7e05feb851e

--- api/src/main/java/org/semanticweb/owlapi/util/CollectionFactory.java
+++ api/src/main/java/org/semanticweb/owlapi/util/CollectionFactory.java
@@ -75,7 +75,7 @@ public class CollectionFactory {
 			this(new ConcurrentHashMap<T, Set<T>>());
 		}
 
-		public SyncSet(Set<T> delegate) {
+		public SyncSet(Collection<T> delegate) {
 			this();
 			for (T d : delegate) {
 				add(d);
@@ -157,6 +157,14 @@ public class CollectionFactory {
 		}
 	}
 
+	/**
+	 * 
+	 * @param source
+	 *            the collection to lazily copy
+	 * @return a lazy defensive copy for source; the source collection will not
+	 *         be copied until a method that modifies the collection gets
+	 *         called, e.g., add(), addAll()
+	 */
 	public static <T> Set<T> getCopyOnRequestSet(Set<T> source) {
 		return new ConditionalCopySet<T>(source);
 	}
@@ -166,23 +174,35 @@ public class CollectionFactory {
 	}
 
 	/**
-	 * a set implementation that uses a delegate set for all read-only
+	 * a set implementation that uses a delegate collection for all read-only
 	 * operations and makes a copy if changes are attempted. Useful for cheap
-	 * defensive copies: no costly rehashing on the original set is made unless
-	 * changes are attempted. Changes are not mirrored back to the original set,
-	 * although changes to the original set BEFORE changes to the copy are
-	 * reflected in the copy. This is not the expected behaviour for regular
-	 * defensive copies, so it needs to be documented; it is irrelevant for
-	 * copies of sets that never change, i.e., signatures of OWL objects.
+	 * defensive copies: no costly rehashing on the original collection is made
+	 * unless changes are attempted. Changes are not mirrored back to the
+	 * original collection, although changes to the original set BEFORE changes
+	 * to the copy are reflected in the copy. If the source collection is not
+	 * supposed to change, then this collection behaves just like a regular
+	 * defensive copy; if the source collection can change, then this collection
+	 * should be built from a cheap copy of the original collection.
+	 * 
+	 * For example, if the source collection is a set, it can be copied into a
+	 * list; the cost of the copy operation from set to list is approximately
+	 * 1/3 of the cost of copying into a new HashSet. This is not efficient if
+	 * the most common operations performed on the copy are contains() or
+	 * containsAll(), since they are more expensive for lists wrt sets; a
+	 * counter for these calls is maintained by the collection, so if a large
+	 * number of contains/containsAll calls takes place, the delegate is turned
+	 * into a regular set.
 	 * 
 	 * This implementation is not threadsafe even if the source set is: there is
-	 * no lock during the copy, and the new set is not threadsafe
+	 * no lock during the copy, and the new set is not threadsafe.
 	 */
 	public static class ConditionalCopySet<T> implements Set<T> {
 		private boolean copyDone = false;
-		private Set<T> delegate;
+		private Collection<T> delegate;
+		private final static int maxContains = 10;
+		private int containsCounter = 0;
 
-		public ConditionalCopySet(Set<T> source) {
+		public ConditionalCopySet(Collection<T> source) {
 			this.delegate = source;
 		}
 
@@ -205,16 +225,32 @@ public class CollectionFactory {
 		public void clear() {
 			if (!copyDone) {
 				copyDone = true;
-				delegate = new HashSet<T>(delegate);
+				delegate = new HashSet<T>();
 			}
 			delegate.clear();
 		}
 
 		public boolean contains(Object arg0) {
+			containsCounter++;
+			if (containsCounter >= maxContains && !copyDone) {
+				// many calls to contains, inefficient if the delegate is not a set
+				if (!(delegate instanceof Set)) {
+					copyDone = true;
+					delegate = new HashSet<T>(delegate);
+				}
+			}
 			return delegate.contains(arg0);
 		}
 
 		public boolean containsAll(Collection<?> arg0) {
+			containsCounter++;
+			if (containsCounter >= maxContains && !copyDone) {
+				// many calls to contains, inefficient if the delegate is not a set
+				if (!(delegate instanceof Set)) {
+					copyDone = true;
+					delegate = new HashSet<T>(delegate);
+				}
+			}
 			return delegate.containsAll(arg0);
 		}
 
@@ -270,13 +306,15 @@ public class CollectionFactory {
 	 * 
 	 * */
 	public static class ThreadSafeConditionalCopySet<T> implements Set<T> {
-		private boolean copyDone = false;
-		private Set<T> delegate;
+		private volatile boolean copyDone = false;
+		private Collection<T> delegate;
 		private final ReadWriteLock lock = new ReentrantReadWriteLock();
 		private final Lock readLock = lock.readLock();
 		private final Lock writeLock = lock.writeLock();
+		private final static int maxContains = 10;
+		private volatile int containsCounter = 0;
 
-		public ThreadSafeConditionalCopySet(Set<T> source) {
+		public ThreadSafeConditionalCopySet(Collection<T> source) {
 			this.delegate = source;
 		}
 
@@ -311,7 +349,7 @@ public class CollectionFactory {
 				writeLock.lock();
 				if (!copyDone) {
 					copyDone = true;
-					delegate = new SyncSet<T>(delegate);
+					delegate = new SyncSet<T>();
 				}
 				delegate.clear();
 			} finally {
@@ -320,6 +358,35 @@ public class CollectionFactory {
 		}
 
 		public boolean contains(Object arg0) {
+			/* note: the check on the number of contains and on 
+			 * the copyDone flag is intentionally not made inside 
+			 * a lock; these operations will not stop other 
+			 * threads from accessing the collection, and the
+			 * lack of synchronization means that a few 
+			 * contains/containsAll calls might not be recorded; 
+			 * the result is that the copy optimization would be 
+			 * delayed. This is not an issue, since maxContains 
+			 * is only a rough estimate.*/
+			if (!copyDone) {
+				containsCounter++;
+				if (containsCounter >= maxContains && !copyDone) {
+					try {
+						writeLock.lock();
+						// many calls to contains, inefficient if the delegate is not a set
+						// copyDone is doublechecked, but here it's protected by the write 
+						//lock as in all other instances in which its value is changed
+						if (!copyDone && !(delegate instanceof Set)) {
+							copyDone = true;
+							delegate = new SyncSet<T>(delegate);
+						}
+						// skip the second portion of the method: no need to reacquire
+						// the lock, it's already a write lock
+						return delegate.contains(arg0);
+					} finally {
+						writeLock.unlock();
+					}
+				}
+			}
 			try {
 				readLock.lock();
 				return delegate.contains(arg0);
@@ -329,6 +396,26 @@ public class CollectionFactory {
 		}
 
 		public boolean containsAll(Collection<?> arg0) {
+			if (!copyDone) {
+				containsCounter++;
+				if (containsCounter >= maxContains && !copyDone) {
+					try {
+						writeLock.lock();
+						// many calls to contains, inefficient if the delegate is not a set
+						// copyDone is doublechecked, but here it's protected by the write 
+						//lock as in all other instances in which its value is changed
+						if (!copyDone && !(delegate instanceof Set)) {
+							copyDone = true;
+							delegate = new SyncSet<T>(delegate);
+						}
+						// skip the second portion of the method: no need to reacquire
+						// the lock, it's already a write lock
+						return delegate.containsAll(arg0);
+					} finally {
+						writeLock.unlock();
+					}
+				}
+			}
 			try {
 				readLock.lock();
 				return delegate.containsAll(arg0);
