ae38cb3f24b52307301d4594e0c2cec6868fd9d6
matthewhorridge <matthewhorridge@02e1a946-7720-0410-8c3b-a7e05feb851e>
2010-12-21 21:16:47 +0000

    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1654 02e1a946-7720-0410-8c3b-a7e05feb851e

---
+++ apibinding/src/test/java/org/semanticweb/owlapi/api/test/BuiltInDatatypesTestCase.java
@@ -0,0 +1,26 @@
+package org.semanticweb.owlapi.api.test;
+
+import junit.framework.TestCase;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.vocab.OWL2Datatype;
+import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
+
+/**
+ * Author: Matthew Horridge<br>
+ * The University of Manchester<br>
+ * Bio-Health Informatics Group<br>
+ * Date: 21/12/2010
+ */
+public class BuiltInDatatypesTestCase extends TestCase {
+
+    public void testBuiltInDatatypes() {
+        try {
+            OWL2Datatype dt = OWL2Datatype.getDatatype(OWLRDFVocabulary.RDF_PLAIN_LITERAL.getIRI());
+            assertNotNull(dt);
+        }
+        catch (RuntimeException e) {
+            fail(e.getMessage());
+        }
+    }
+}
