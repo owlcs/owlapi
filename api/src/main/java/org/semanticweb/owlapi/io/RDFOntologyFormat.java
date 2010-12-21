9ebd70805b8af3dfcb7ba427febb21943c5ddf0b
matthewhorridge <matthewhorridge@02e1a946-7720-0410-8c3b-a7e05feb851e>
2010-12-21 18:23:11 +0000

    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1651 02e1a946-7720-0410-8c3b-a7e05feb851e

--- api/src/main/java/org/semanticweb/owlapi/io/RDFOntologyFormat.java
+++ api/src/main/java/org/semanticweb/owlapi/io/RDFOntologyFormat.java
@@ -14,7 +14,7 @@ import java.util.Set;
  */
 public abstract class RDFOntologyFormat extends PrefixOWLOntologyFormat {
 
-    private boolean addMissingTypes = false;
+    private boolean addMissingTypes = true;
 
     /**
      * Determines if untyped entities should automatically be typed (declared) during rendering.  (This is a hint to an RDF
