d0ac3aa5d46b313732e0927af30a49e772d7d2e9
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2011-01-12 19:30:28 +0000

    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1710 02e1a946-7720-0410-8c3b-a7e05feb851e

---
+++ apibinding/src/test/java/org/semanticweb/owlapi/api/test/AnonymousTurtleAssertionTestCase.java
@@ -0,0 +1,59 @@
+package org.semanticweb.owlapi.api.test;
+/*
+ * Copyright (C) 2008, University of Manchester
+ *
+ * Modifications to the initial code base are copyright of their
+ * respective authors, or their employers as appropriate.  Authorship
+ * of the modifications may be determined from the ChangeLog placed at
+ * the end of this file.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+/**
+ * Author: Matthew Horridge<br> The University Of Manchester<br> Information Management Group<br> Date:
+ * 30-Jul-2008<br><br>
+ */
+public class AnonymousTurtleAssertionTestCase extends AbstractFileRoundTrippingTestCase {
+
+    @Override
+	protected String getFileName() {
+        return "testBlankNodesAssertions.ttl";
+    }
+
+    @Override
+    public void testTurtle() throws Exception {
+    	// TODO Auto-generated method stub
+    	super.testTurtle();
+    }
+    
+    @Override
+    public void testFunctionalSyntax() throws Exception {
+    }
+    @Override
+    public void testManchesterOWLSyntax() throws Exception {
+    
+    }
+    @Override
+    public void testOWLXML() throws Exception {
+    
+    }
+    @Override
+    public void testRDFXML() throws Exception {
+    
+    }
+    
+}
