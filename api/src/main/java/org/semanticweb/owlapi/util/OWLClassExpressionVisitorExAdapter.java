77b7c8bb851e24d15e1f3aabcc00b2f5aaf73e4d
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2011-10-12 08:06:47 +0000

    new adapter
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1853 02e1a946-7720-0410-8c3b-a7e05feb851e

---
+++ api/src/main/java/org/semanticweb/owlapi/util/OWLClassExpressionVisitorExAdapter.java
@@ -0,0 +1,147 @@
+/*
+ * This file is part of the OWL API.
+ *
+ * The contents of this file are subject to the LGPL License, Version 3.0.
+ *
+ * Copyright (C) 2011, The University of Manchester
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ *
+ * Alternatively, the contents of this file may be used under the terms of the Apache License, Version 2.0
+ * in which case, the provisions of the Apache License Version 2.0 are applicable instead of those above.
+ *
+ * Copyright 2011, University of Manchester
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.semanticweb.owlapi.util;
+
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpressionVisitor;
+import org.semanticweb.owlapi.model.OWLClassExpressionVisitorEx;
+import org.semanticweb.owlapi.model.OWLDataAllValuesFrom;
+import org.semanticweb.owlapi.model.OWLDataExactCardinality;
+import org.semanticweb.owlapi.model.OWLDataHasValue;
+import org.semanticweb.owlapi.model.OWLDataMaxCardinality;
+import org.semanticweb.owlapi.model.OWLDataMinCardinality;
+import org.semanticweb.owlapi.model.OWLDataSomeValuesFrom;
+import org.semanticweb.owlapi.model.OWLObjectAllValuesFrom;
+import org.semanticweb.owlapi.model.OWLObjectComplementOf;
+import org.semanticweb.owlapi.model.OWLObjectExactCardinality;
+import org.semanticweb.owlapi.model.OWLObjectHasSelf;
+import org.semanticweb.owlapi.model.OWLObjectHasValue;
+import org.semanticweb.owlapi.model.OWLObjectIntersectionOf;
+import org.semanticweb.owlapi.model.OWLObjectMaxCardinality;
+import org.semanticweb.owlapi.model.OWLObjectMinCardinality;
+import org.semanticweb.owlapi.model.OWLObjectOneOf;
+import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom;
+import org.semanticweb.owlapi.model.OWLObjectUnionOf;
+
+
+/**
+ * Author: Matthew Horridge<br>
+ * The University Of Manchester<br>
+ * Bio-Health Informatics Group<br>
+ * Date: 22-Nov-2006<br><br>
+ */
+@SuppressWarnings("unused")
+public class OWLClassExpressionVisitorExAdapter<O> implements OWLClassExpressionVisitorEx<O> {
+
+	public O visit(OWLClass ce) {
+
+		return null;
+	}
+
+	public O visit(OWLObjectIntersectionOf ce) {
+
+		return null;
+	}
+
+	public O visit(OWLObjectUnionOf ce) {
+		return null;
+	}
+
+	public O visit(OWLObjectComplementOf ce) {
+		return null;
+	}
+
+	public O visit(OWLObjectSomeValuesFrom ce) {
+		return null;
+	}
+
+	public O visit(OWLObjectAllValuesFrom ce) {
+		return null;
+	}
+
+	public O visit(OWLObjectHasValue ce) {
+		return null;
+	}
+
+	public O visit(OWLObjectMinCardinality ce) {
+		return null;
+	}
+
+	public O visit(OWLObjectExactCardinality ce) {
+		return null;
+	}
+
+	public O visit(OWLObjectMaxCardinality ce) {
+		return null;
+	}
+
+	public O visit(OWLObjectHasSelf ce) {
+		return null;
+	}
+
+	public O visit(OWLObjectOneOf ce) {
+		return null;
+	}
+
+	public O visit(OWLDataSomeValuesFrom ce) {
+		return null;
+	}
+
+	public O visit(OWLDataAllValuesFrom ce) {
+		return null;
+	}
+
+	public O visit(OWLDataHasValue ce) {
+		return null;
+	}
+
+	public O visit(OWLDataMinCardinality ce) {
+		return null;
+	}
+
+	public O visit(OWLDataExactCardinality ce) {
+		return null;
+	}
+
+	public O visit(OWLDataMaxCardinality ce) {
+		return null;
+	}
+
+}
