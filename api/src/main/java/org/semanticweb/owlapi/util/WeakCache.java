4b5cbb81ab0f40d34cf54a8d66d6e118108dcff5
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2012-02-06 11:20:24 +0000

    fix for 3479677 - invalid qnames, weak reference caching for default prefix manager
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1924 02e1a946-7720-0410-8c3b-a7e05feb851e

--- api/src/main/java/org/semanticweb/owlapi/util/WeakCache.java
+++ api/src/main/java/org/semanticweb/owlapi/util/WeakCache.java
@@ -22,6 +22,19 @@ public class WeakCache<K> {
 		prefixCache.put(s, new WeakReference<K>(s));
 		return s;
 	}
+	/**
+	 * @param k the key to check
+	 * @return true if the cache contains k as a key; note that, due to the nature of this cache, by the time the method returns the key may no longer be in the map.*/
+	public boolean contains(K k) {
+		WeakReference<K> w = prefixCache.get(k);
+		if (w != null) {
+			K toReturn = w.get();
+			if (toReturn != null) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	public void clear() {
 		prefixCache.clear();
