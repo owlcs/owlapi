96d71241fe4a9f2a35e9243460f5c9d9c475d3ba
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2012-03-12 11:01:11 +0000

    profiles moved to fix maven issues
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1930 02e1a946-7720-0410-8c3b-a7e05feb851e

---
+++ api/src/main/java/org/semanticweb/owlapi/profiles/LastPropertyInChainNotInImposedRange.java
@@ -0,0 +1,86 @@
+/*
+ * This file is part of the OWL API.
+ *
+ * The contents of this file are subject to the LGPL License, Version 3.0.
+ *
+ * Copyright (C) 2011, The University of Manchester
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ *
+ * Alternatively, the contents of this file may be used under the terms of the Apache License, Version 2.0
+ * in which case, the provisions of the Apache License Version 2.0 are applicable instead of those above.
+ *
+ * Copyright 2011, University of Manchester
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.semanticweb.owlapi.profiles;
+
+import org.semanticweb.owlapi.model.OWLObjectPropertyRangeAxiom;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLSubPropertyChainOfAxiom;
+
+/**
+ * Author: Matthew Horridge<br>
+ * The University of Manchester<br>
+ * Information Management Group<br>
+ * Date: 03-Aug-2009
+ */
+@SuppressWarnings("javadoc")
+public class LastPropertyInChainNotInImposedRange extends OWLProfileViolation implements OWL2ELProfileViolation {
+
+    private final OWLSubPropertyChainOfAxiom axiom;
+
+    private final OWLObjectPropertyRangeAxiom rangeAxiom;
+
+    public LastPropertyInChainNotInImposedRange(OWLOntology ontology, OWLSubPropertyChainOfAxiom axiom, OWLObjectPropertyRangeAxiom rangeAxiom) {
+        super(ontology, axiom);
+        this.axiom = axiom;
+        this.rangeAxiom = rangeAxiom;
+    }
+
+    public void accept(OWL2ELProfileViolationVisitor visitor) {
+        visitor.visit(this);
+    }
+
+    public OWLSubPropertyChainOfAxiom getOWLSubPropertyChainOfAxiom() {
+        return axiom;
+    }
+
+    public OWLObjectPropertyRangeAxiom getOWLObjectPropertyRangeAxiom() {
+        return rangeAxiom;
+    }
+
+    @Override
+	public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Last property in chain not in imposed data range: ");
+        sb.append(axiom);
+        sb.append(" for data range ");
+        sb.append(rangeAxiom);
+        return sb.toString();
+    }
+}
