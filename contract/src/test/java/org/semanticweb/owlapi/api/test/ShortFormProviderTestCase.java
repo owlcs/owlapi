5d7633552771bbb56933843545efe615db3bcb67
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2012-08-30 18:29:38 +0000

    fix and test for QNameShortFormProvider - it fixes an issue where a null pointer exception would be thrown under some conditions.
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@2042 02e1a946-7720-0410-8c3b-a7e05feb851e

---
+++ contract/src/test/java/org/semanticweb/owlapi/api/test/ShortFormProviderTestCase.java
@@ -0,0 +1,24 @@
+package org.semanticweb.owlapi.api.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.util.QNameShortFormProvider;
+
+@SuppressWarnings("javadoc")
+public class ShortFormProviderTestCase {
+
+    @Test
+    public void shouldFindShortForm() {
+        OWLClass c = OWLManager
+                .getOWLDataFactory()
+                .getOWLClass(
+                        IRI.create("http://www.ebi.ac.uk/fgpt/ontologies/test/TEST_00001> test:TEST_00001"));
+        QNameShortFormProvider shortener = new QNameShortFormProvider();
+        String shortform = shortener.getShortForm(c);
+        assertEquals("test:TEST_00001", shortform);
+    }
+}
