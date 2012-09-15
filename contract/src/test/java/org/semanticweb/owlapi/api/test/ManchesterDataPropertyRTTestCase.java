615856f1d2760e367e028dcada2b41b49b7fd179
ignazio1977 <ignazio1977@02e1a946-7720-0410-8c3b-a7e05feb851e>
2012-09-15 19:31:00 +0000

    Test for 3174734 - dataproperty issue no longer replicable
    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@2054 02e1a946-7720-0410-8c3b-a7e05feb851e

---
+++ contract/src/test/java/org/semanticweb/owlapi/api/test/ManchesterDataPropertyRTTestCase.java
@@ -0,0 +1,45 @@
+package org.semanticweb.owlapi.api.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.StringDocumentSource;
+import org.semanticweb.owlapi.io.StringDocumentTarget;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+
+@SuppressWarnings("javadoc")
+public class ManchesterDataPropertyRTTestCase {
+    @Test
+    public void shouldRoundTrip() throws OWLOntologyCreationException,
+    OWLOntologyStorageException, IOException {
+        // given
+        OWLDataFactory factory = OWLManager.getOWLDataFactory();
+        String NS = "http://protege.org/ontologies";
+        OWLDataProperty p = factory.getOWLDataProperty(IRI.create(NS + "#p"));
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+        OWLOntology ontology = manager.createOntology(IRI.create(NS));
+        manager.addAxiom(ontology, factory.getOWLDeclarationAxiom(p));
+        String saved = saveOntology(ontology);
+        // when
+        ontology = OWLManager.createOWLOntologyManager()
+                .loadOntologyFromOntologyDocument(new StringDocumentSource(saved));
+        // then
+        assertTrue(ontology.containsDataPropertyInSignature(p.getIRI()));
+    }
+
+    private String saveOntology(OWLOntology o) throws IOException,
+    OWLOntologyStorageException {
+        StringDocumentTarget target = new StringDocumentTarget();
+        o.getOWLOntologyManager().saveOntology(o, target);
+        return target.toString();
+    }
+}
