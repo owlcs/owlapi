41d0b4f4523f9f703faa3df5a7d0b85fcb6da17f
ignazio1977 <ignazio1977@users.sourceforge.net>
2012-11-04 13:35:46 +0000

    Refactored common comparators.

---
+++ api/src/main/java/org/semanticweb/owlapi/util/OWLEntityIRIComparator.java
@@ -0,0 +1,16 @@
+package org.semanticweb.owlapi.util;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+import org.semanticweb.owlapi.model.OWLEntity;
+
+/** Comparator that uses IRI ordering to order entities. */
+public class OWLEntityIRIComparator implements Comparator<OWLEntity>, Serializable {
+    private static final long serialVersionUID = 30402L;
+
+    @Override
+    public int compare(OWLEntity o1, OWLEntity o2) {
+        return o1.getIRI().compareTo(o2.getIRI());
+    }
+}
