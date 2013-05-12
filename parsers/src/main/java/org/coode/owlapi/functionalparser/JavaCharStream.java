94a319919e95df51ba278f2b72eb3b3cef163278
ignazio1977 <ignazio1977@users.sourceforge.net>
2013-05-12 10:47:42 +0100

    updated fix for utf8

---
+++ parsers/src/main/java/org/coode/owlapi/functionalparser/JavaCharStream.java
@@ -0,0 +1,22 @@
+package org.coode.owlapi.functionalparser;
+
+/** Wrapper for the common JavaCharStream for all parsers. This is needed because
+ * we have a request to support UTF-8 files that have an initial BOM. UTF-8 does
+ * not need BOMs but some tools insist adding them. Java on the other hand won't
+ * support it, and JavaCC does not have an override for this. The result is that
+ * the support must be added by hand, and it's easier to have it in one place.
+ * This stops JavaCC from rebuilding the wrong files and minimizes duplicated
+ * code. */
+@SuppressWarnings("javadoc")
+public class JavaCharStream extends uk.ac.manchester.cs.JavaCharStream {
+    /** Constructor. */
+    public JavaCharStream(java.io.Reader dstream, int startline, int startcolumn) {
+        super(dstream, startline, startcolumn, 4096);
+    }
+
+    /** Constructor. */
+    public JavaCharStream(java.io.InputStream dstream, String encoding, int startline,
+            int startcolumn) throws java.io.UnsupportedEncodingException {
+        super(dstream, encoding, startline, startcolumn, 4096);
+    }
+}
