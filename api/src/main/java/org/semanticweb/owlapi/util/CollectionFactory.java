605bcd54f25077d8d8aad2362d968a861165ceb3
ignazio1977 <ignazio1977@users.sourceforge.net>
2013-05-14 21:47:56 +0100

    WeakIndexCache vulnerability fixed

--- api/src/main/java/org/semanticweb/owlapi/util/CollectionFactory.java
+++ api/src/main/java/org/semanticweb/owlapi/util/CollectionFactory.java
@@ -38,6 +38,7 @@
  */
 package org.semanticweb.owlapi.util;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -47,6 +48,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.WeakHashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
@@ -99,7 +101,13 @@ public class CollectionFactory {
         return new HashMap<K, V>();
     }
 
-    /** @param elements
+    /** @return a new weak hashmap wrapped as a synchronized map */
+    public static <K, V> Map<K, WeakReference<V>> createSyncWeakMap() {
+        return Collections.synchronizedMap(new WeakHashMap<K, WeakReference<V>>());
+    }
+
+    /**
+     * @param elements
      *            values to add to the set
      * @return fresh non threadsafe set */
     public static <T> Set<T> createSet(T... elements) {
