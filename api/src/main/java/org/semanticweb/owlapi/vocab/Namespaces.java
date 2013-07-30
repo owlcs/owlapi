caff5fd939114a321dc956d2023e8acac2525202
matthewhorridge <matthew.horridge@stanford.edu>
2013-07-30 21:53:44 +0100

    Added more well known namespaces, removed non-explanatory comments

--- api/src/main/java/org/semanticweb/owlapi/vocab/Namespaces.java
+++ api/src/main/java/org/semanticweb/owlapi/vocab/Namespaces.java
@@ -44,105 +44,66 @@ package org.semanticweb.owlapi.vocab;
  * Date: 13-Dec-2006<br>
  * <br> */
 public enum Namespaces {
+    //@formatter:off
     // OWL2XML("http://www.w3.org/2006/12/owl2-xml#"),
     /** The OWL 2 namespace is here for legacy reasons. */
-    OWL2("owl2", "http://www.w3.org/2006/12/owl2#", false, false),
-    /** legacy */
-    OWL11XML("owl11xml", "http://www.w3.org/2006/12/owl11-xml#", false, false),
-    /** The OWL 1.1 namespace is here for legacy reasons. */
-    OWL11("owl11", "http://www.w3.org/2006/12/owl11#", false, false),
-    /** OWL namespace */
-    OWL("owl", "http://www.w3.org/2002/07/owl#", true, true),
-    /** RDFS namespace */
-    RDFS("rdfs", "http://www.w3.org/2000/01/rdf-schema#", true, true),
-    /** RDF namespace */
-    RDF("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#", true, true),
-    /** XSD namespace */
-    XSD("xsd", "http://www.w3.org/2001/XMLSchema#", true, true),
-    /** XML namespace */
-    XML("xml", "http://www.w3.org/XML/1998/namespace", true, false),
-    /** SWRL namespace */
-    SWRL("swrl", "http://www.w3.org/2003/11/swrl#", true, false),
-    /** SWRLB namespace */
-    SWRLB("swrlb", "http://www.w3.org/2003/11/swrlb#", true, false),
-    /** SKOS namespace */
-    SKOS("skos", "http://www.w3.org/2004/02/skos/core#", true, false),
-    
-    // Further namespaces from the RDFa Core Initial Context
-    // http://www.w3.org/2011/rdfa-context/rdfa-1.1
-    
-    /**GRDDL namespace*/
-    GRDDL("grddl", "http://www.w3.org/2003/g/data-view#", true, false),
-    /**MA namespace*/
-    MA("ma", "http://www.w3.org/ns/ma-ont#", true, false),
-    /**PROV namespace*/
-    PROV("prov", "http://www.w3.org/ns/prov#", true, false),
-    /**RDFA namespace*/
-    RDFA("rdfa", "http://www.w3.org/ns/rdfa#", true, false),
-    /**RIF namespace*/
-    RIF("rif", "http://www.w3.org/2007/rif#", true, false),
-    /**R2RML namespace*/
-    R2RML("rr", "http://www.w3.org/ns/r2rml#", true, false),
-    /**SD namespace*/
-    SD("sd", "http://www.w3.org/ns/sparql-service-description#", true, false),
-    /**SKOSXL namespace*/
-    SKOSXL("skosxl", "http://www.w3.org/2008/05/skos-xl#", true, false),
-    /**POWDER namespace*/
-    POWDER("wdr", "http://www.w3.org/2007/05/powder#", true, false),
-    /**VOID namespace*/
-    VOID("void", "http://rdfs.org/ns/void#", true, false),
-    /**POWDER-S namespace*/
-    POWDERS("wdrs", "http://www.w3.org/2007/05/powder-s#", true, false),
-    /**XHV namespace*/
-    XHV("xhv", "http://www.w3.org/1999/xhtml/vocab#", true, false),
-    
-    
-    /**ORG namespace*/
-    ORG("org", "http://www.w3.org/ns/org#", true, false),
-    /**GLDP namespace*/
-    GLDP("gldp", "http://www.w3.org/ns/people#", true, false),
-    /**CNT namespace*/
-    CNT("cnt", "http://www.w3.org/2008/content#", true, false),
-    /**DCAT namespace*/
-    DCAT("dcat", "http://www.w3.org/ns/dcat#", true, false),
-    /**EARL namespace*/
-    EARL("earl", "http://www.w3.org/ns/earl#", true, false),
-    /**HT namespace*/
-    HT("ht", "http://www.w3.org/2006/http#", true, false),
-    /**PTR namespace*/
-    PTR("ptr", "http://www.w3.org/2009/pointers#", true, false),
-    
-    
-    // Other widely used Semantic Web prefixes
-    
-    /**CC namespace*/
-    CC("cc", "http://creativecommons.org/ns#", true, false),
-    /**CTAG namespace*/
-    CTAG("ctag", "http://commontag.org/ns#", true, false),
-    /**DCTERMS namespace*/
-    DCTERMS("dcterms", "http://purl.org/dc/terms/", true, false),
-    /**DC elements namespace*/
-    DC("dc", "http://purl.org/dc/elements/1.1/", true, false),
-    /**FOAF namespace*/
-    FOAF("foaf", "http://xmlns.com/foaf/0.1/", true, false),
-    /**GR namespace*/
-    GR("gr", "http://purl.org/goodrelations/v1#", true, false),
-    /**ICAL namespace*/
-    ICAL("ical", "http://www.w3.org/2002/12/cal/icaltzd#", true, false),
-    /**OG namespace*/
-    OG("og", "http://ogp.me/ns#", true, false),
-    /**REV namespace*/
-    REV("rev", "http://purl.org/stuff/rev#", true, false),
-    /**SIOC namespace*/
-    SIOC("sioc", "http://rdfs.org/sioc/ns#", true, false),
-    /**VCARD namespace*/
-    VCARD("vcard", "http://www.w3.org/2006/vcard/ns#", true, false),
-    /**SCHEMA namespace*/
-    SCHEMA("schema", "http://schema.org/", true, false),
-    
-    /**GEO namespace*/
-    GEO("geo", "http://www.w3.org/2003/01/geo/wgs84_pos#", true, false);
-
+    OWL2    ("owl2",    "http://www.w3.org/2006/12/owl2#",                  false, false),
+    /** legacy */                                                           
+    OWL11XML("owl11xml", "http://www.w3.org/2006/12/owl11-xml#",            false, false),
+    /** The OWL 1.1 namespace is here for legacy reasons. */                
+    OWL11   ("owl11",   "http://www.w3.org/2006/12/owl11#",                 false, false),
+    OWL     ("owl",     "http://www.w3.org/2002/07/owl#",                   true,  true),
+    RDFS    ("rdfs",    "http://www.w3.org/2000/01/rdf-schema#",            true,  true),
+    RDF     ("rdf",     "http://www.w3.org/1999/02/22-rdf-syntax-ns#",      true,  true),
+    XSD     ("xsd",     "http://www.w3.org/2001/XMLSchema#",                true,  true),
+    XML     ("xml",     "http://www.w3.org/XML/1998/namespace",             true,  false),
+    SWRL    ("swrl",    "http://www.w3.org/2003/11/swrl#",                  true,  false),
+    SWRLB   ("swrlb",   "http://www.w3.org/2003/11/swrlb#",                 true,  false),
+    SKOS    ("skos",    "http://www.w3.org/2004/02/skos/core#",             true,  false),
+    // Further namespaces from the RDFa Core Initial Context                       
+    // http://www.w3.org/2011/rdfa-context/rdfa-1.1                                
+    GRDDL   ("grddl",   "http://www.w3.org/2003/g/data-view#",              true,  false),
+    MA      ("ma",      "http://www.w3.org/ns/ma-ont#",                     true,  false),
+    PROV    ("prov",    "http://www.w3.org/ns/prov#",                       true,  false),
+    RDFA    ("rdfa",    "http://www.w3.org/ns/rdfa#",                       true,  false),
+    RIF     ("rif",     "http://www.w3.org/2007/rif#",                      true,  false),
+    R2RML   ("rr",      "http://www.w3.org/ns/r2rml#",                      true,  false),
+    SD      ("sd",      "http://www.w3.org/ns/sparql-service-description#", true,  false),
+    SKOSXL  ("skosxl",  "http://www.w3.org/2008/05/skos-xl#",               true,  false),
+    POWDER  ("wdr",     "http://www.w3.org/2007/05/powder#",                true,  false),
+    VOID    ("void",    "http://rdfs.org/ns/void#",                         true,  false),
+    POWDERS ("wdrs",    "http://www.w3.org/2007/05/powder-s#",              true,  false),
+    XHV     ("xhv",     "http://www.w3.org/1999/xhtml/vocab#",              true,  false),
+    ORG     ("org",     "http://www.w3.org/ns/org#",                        true,  false),
+    GLDP    ("gldp",    "http://www.w3.org/ns/people#",                     true,  false),
+    CNT     ("cnt",     "http://www.w3.org/2008/content#",                  true,  false),
+    DCAT    ("dcat",    "http://www.w3.org/ns/dcat#",                       true,  false),
+    EARL    ("earl",    "http://www.w3.org/ns/earl#",                       true,  false),
+    HT      ("ht",      "http://www.w3.org/2006/http#",                     true,  false),
+    PTR     ("ptr",     "http://www.w3.org/2009/pointers#",                 true,  false),
+    // Other widely used Semantic Web prefixes                                     
+    CC      ("cc",      "http://creativecommons.org/ns#",                   true,  false),
+    CTAG    ("ctag",    "http://commontag.org/ns#",                         true,  false),
+    DCTERMS ("dcterms", "http://purl.org/dc/terms/",                        true,  false),
+    DC      ("dc",      "http://purl.org/dc/elements/1.1/",                 true,  false),
+    FOAF    ("foaf",    "http://xmlns.com/foaf/0.1/",                       true,  false),
+    GR      ("gr",      "http://purl.org/goodrelations/v1#",                true,  false),
+    ICAL    ("ical",    "http://www.w3.org/2002/12/cal/icaltzd#",           true,  false),
+    OG      ("og",      "http://ogp.me/ns#",                                true,  false),
+    REV     ("rev",     "http://purl.org/stuff/rev#",                       true,  false),
+    SIOC    ("sioc",    "http://rdfs.org/sioc/ns#",                         true,  false),
+    VCARD   ("vcard",   "http://www.w3.org/2006/vcard/ns#",                 true,  false),
+    SCHEMA  ("schema",  "http://schema.org/",                               true,  false),
+    GEO     ("geo",     "http://www.w3.org/2003/01/geo/wgs84_pos#",         true,  false),
+    SC      ("sc",      "http://purl.org/science/owl/sciencecommons/",      true,  false),
+    FB      ("fb",      "http://rdf.freebase.com/ns/",                      false, true),
+    GEONAMES("geonames","http://www.geonames.org/ontology#",                false, true),
+    // DBpedia                                                              
+    DBPEDIA("dbpedia",  "http://dbpedia.org/resource/",                     true, false),
+    DBP    ("dbp",      "http://dbpedia.org/property/",                     true, false),
+    DBO    ("dbo",      "http://dbpedia.org/ontology/",                     true, false),
+    YAGO   ("yago",     "http://dbpedia.org/class/yago/",                   true, false);
+    //@formatter:on
     final String prefix;
     final String ns;
     final boolean inUse;
@@ -161,9 +122,7 @@ public enum Namespaces {
         return prefix;
     }
 
-    /**
-     * @return The prefix IRI which matches the prefix name.
-     */
+    /** @return The prefix IRI which matches the prefix name. */
     public String getPrefixIRI() {
         return ns;
     }
