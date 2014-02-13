86be5a206f11d1618cd7b9d50e030f26d2fbf3ba
ignazio <ignazio.palmisano@gmail.com>
2014-02-13 20:48:00 +0000

    corrected restriction hierarchy

--- api/src/main/java/org/semanticweb/owlapi/model/OWLDataRestriction.java
+++ api/src/main/java/org/semanticweb/owlapi/model/OWLDataRestriction.java
@@ -45,10 +45,10 @@ import javax.annotation.Nonnull;
  * >Object Property Restriction</a> or <a href=
  * "http://www.w3.org/TR/2009/REC-owl2-syntax-20091027/#Data_Property_Restrictions"
  * >Data Property Restriction</a>) in the OWL 2 specification.
- *
+ * 
  * @author Matthew Horridge, The University Of Manchester, Bio-Health
  *         Informatics Group Date: 24-Oct-2006 */
-public interface OWLDataRestriction extends OWLAnonymousClassExpression {
+public interface OWLDataRestriction extends OWLRestriction {
     /** Gets the property/properties that the restriction acts along depending on
      * R being a scalar or collection type.
      * 
