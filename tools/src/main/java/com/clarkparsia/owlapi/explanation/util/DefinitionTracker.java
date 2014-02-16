a331415166a3eaddc7f7daac02635d94383daa25
ignazio <ignazio.palmisano@gmail.com>
2014-02-16 16:48:15 +0000

    package-info javadoc drafted

--- tools/src/main/java/com/clarkparsia/owlapi/explanation/util/DefinitionTracker.java
+++ tools/src/main/java/com/clarkparsia/owlapi/explanation/util/DefinitionTracker.java
@@ -1,6 +1,6 @@
 /* This file is part of the OWL API.
  * The contents of this file are subject to the LGPL License, Version 3.0.
- * Copyright 2014, The University of Manchester
+ * Copyright 2011, Clark & Parsia, LLC
  * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
