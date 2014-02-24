d60fa9c77888e6e331b64692c8a7443ce5bd31da
Matthew Horridge <matthew.horridge@stanford.edu>
2014-02-24 11:50:59 -0800

    Tidying of OntologyIRIShortFormProvider and test cases

--- api/src/main/java/org/semanticweb/owlapi/util/OntologyIRIShortFormProvider.java
+++ api/src/main/java/org/semanticweb/owlapi/util/OntologyIRIShortFormProvider.java
@@ -39,49 +39,168 @@
 package org.semanticweb.owlapi.util;
 
 import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.vocab.Namespaces;
 
-/** @author Matthew Horridge, The University Of Manchester, Bio-Health Informatics
- *         Group, Date: 23-Jan-2008 */
+/**
+ * @author Matthew Horridge, The University Of Manchester, Bio-Health Informatics
+ *         Group, Date: 23-Jan-2008
+ */
 public class OntologyIRIShortFormProvider implements IRIShortFormProvider {
-    /** @param ont
-     *            ontology to use
-     * @return short form of the ontology IRI */
+
+    /*
+     *  IMPLEMENTATION NOTE.  I've avoided using Pattern (regexps) here so that this code can be compiled
+     *  with the GWT compiler.
+     */
+
+
+    private static final String OWL_EXTENSION = ".owl";
+
+    private static final String RDF_EXTENSION = ".rdf";
+
+    private static final String XML_EXTENSION = ".xml";
+
+    private static final String OBO_EXTENSION = ".obo";
+
+    private static final String [] EXTENSIONS = {OWL_EXTENSION, RDF_EXTENSION, XML_EXTENSION, OBO_EXTENSION};
+
+    private static final Map<IRI, String> wellKnownShortForms;
+
+    static {
+        Map<IRI, String> map = new HashMap<IRI, String>();
+        for(Namespaces ns : Namespaces.values()) {
+            final String iriPrefix = ns.getPrefixIRI();
+            final String iri;
+            if(iriPrefix.endsWith("#") || iriPrefix.endsWith("/")) {
+                iri = iriPrefix.substring(0, iriPrefix.length() - 1);
+            }
+            else {
+                iri = iriPrefix;
+            }
+            map.put(IRI.create(iri), ns.getPrefixName().toLowerCase());
+            map.put(IRI.create(iri + "/"), ns.getPrefixName().toLowerCase());
+        }
+        wellKnownShortForms = Collections.unmodifiableMap(map);
+    }
+
+
+    /**
+     * @param ont ontology to use
+     * @return short form of the ontology IRI
+     */
     public String getShortForm(OWLOntology ont) {
         if (!ont.isAnonymous()) {
             return getShortForm(ont.getOntologyID().getOntologyIRI());
-        } else {
+        }
+        else {
             return ont.getOntologyID().toString();
         }
     }
 
     @Override
     public String getShortForm(IRI iri) {
-        // XXX I suspect this is broken
-        String shortForm = iri.toString();
+        String wellKnownShortForm = getWellKnownShortForm(iri);
+        if(wellKnownShortForm != null) {
+            return wellKnownShortForm;
+        }
         URI uri = iri.toURI();
         String path = uri.getPath();
+        String shortForm = null;
         if (path != null && path.length() > 0) {
             StringTokenizer tokenizer = new StringTokenizer(path, "/", false);
-            String lastPathComponent = "";
+            String candidatePathElement = "";
             while (tokenizer.hasMoreTokens()) {
                 String tok = tokenizer.nextToken();
-                if (tok.length() > 0) {
-                    lastPathComponent = tok;
+                if (isCandidatePathElement(tok)) {
+                    candidatePathElement = stripExtensionIfPresent(tok);
                 }
             }
-            if (lastPathComponent.endsWith(".owl")) {
-                shortForm = lastPathComponent
-                        .substring(0, lastPathComponent.length() - 4);
-            } else {
-                shortForm = lastPathComponent;
-            }
-        } else if (uri.getHost() != null) {
+            shortForm = candidatePathElement;
+        }
+        else if (uri.getHost() != null) {
+            shortForm = iri.toString();
+        }
+        else {
             shortForm = iri.toString();
         }
         return shortForm;
+    }
+
+    private String getWellKnownShortForm(IRI iri) {
+        String wellKnownShortForm = wellKnownShortForms.get(iri);
+        if(wellKnownShortForm != null) {
+            return wellKnownShortForm;
+        }
+        return null;
+    }
+
+    /**
+     * Removes commonly used file name extensions to make the resulting short form look nicer.
+     * @param shortForm The short form.
+     * @return The short form with the extension removed if it was present, or the original short form if no
+     * extension was present.
+     */
+    private static String stripExtensionIfPresent(String shortForm) {
+        String lowerCaseShortForm = shortForm.toLowerCase();
+        for(String extension : EXTENSIONS) {
+            if (lowerCaseShortForm.endsWith(extension)) {
+                return shortForm.substring(0, extension.length() - 1);
+            }
+        }
+        return shortForm;
+    }
+
+
+    /**
+     * Determines if the specified path element is a candidate short form.
+     *
+     * @param pathElement The path element to test.  Not {@code null}.
+     * @return {@code true} if the specified path element is a candidate short form, otherwise {@code false}.
+     */
+    private static boolean isCandidatePathElement(String pathElement) {
+        return !pathElement.isEmpty() && !isVersionString(pathElement);
+    }
+
+    /**
+     * Determines if the specified string is a version number string.  A version string is a sequence of
+     * digits and periods.
+     *
+     * @param s The string to test for.
+     * @return {@code true} if the string is a version string, otherwise {@code false}.
+     */
+    private static boolean isVersionString(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!isVersionStringChar(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Determines if the specified character is a version string character (either a digit or a period).
+     *
+     * @param ch The character to test for.
+     * @return {@code true} of the specified char is a version string char, otherwise {@code false}.
+     */
+    private static boolean isVersionStringChar(char ch) {
+        return isDigit(ch) || ch == '.' || ch == 'v';
+    }
+
+    /**
+     * Determines if the specified char is a digit.
+     *
+     * @param ch The char to test for.
+     * @return {@code true} if the specified char is a digit, otherwise {@code false}.
+     */
+    private static boolean isDigit(char ch) {
+        return ch >= '0' && ch <= '9';
     }
 }
