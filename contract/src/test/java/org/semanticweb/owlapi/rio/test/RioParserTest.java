09e2f9ead4e68f827bae56b0c42e315473d1ecf2
Simon Spero <sesuncedu@gmail.com>
2014-03-08 19:35:55 -0500

    Change the rdfxml test file to remove apparent errors.      <owl:Class rdf:resource="Dead"/>    replaced with        <owl:allValuesFrom rdf:resource="Dead"/>.
         <rdfs:range rdfs:Datatype="http://www.w3.org/2001/XMLSchema#nonNegativeInteger"/>
       replaced with
         <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#nonNegativeInteger"/>
    
      Axiom count in RIoTest reduced from 93 to 92.
      No idea if that is correct.

--- contract/src/test/java/org/semanticweb/owlapi/rio/test/RioParserTest.java
+++ contract/src/test/java/org/semanticweb/owlapi/rio/test/RioParserTest.java
@@ -80,7 +80,6 @@ public class RioParserTest extends TestBase {
      * Test method for
      * {@link org.semanticweb.owlapi.rio.RioParserImpl#parse(org.semanticweb.owlapi.io.OWLOntologyDocumentSource, org.semanticweb.owlapi.model.OWLOntology)}
      */
-    @Ignore
     @Test
     public void testParsePrimer() throws OWLParserException, IOException,
             OWLOntologyCreationException {
@@ -89,7 +88,7 @@ public class RioParserTest extends TestBase {
         OWLOntologyFormat owlapiOntologyFormat = owlapiParser.parse(
                 new StreamDocumentSource(this.getClass().getResourceAsStream(
                         "/primer.rdfxml.xml")), owlapiOntologyPrimer);
-        assertEquals(93, owlapiOntologyPrimer.getAxiomCount());
+        assertEquals(92, owlapiOntologyPrimer.getAxiomCount());
         assertEquals(new RDFXMLOntologyFormat(), owlapiOntologyFormat);
         RioParserImpl rioParser = new RioParserImpl(
                 new RDFXMLOntologyFormatFactory());
@@ -103,7 +102,7 @@ public class RioParserTest extends TestBase {
         assertEquals(new RioRDFXMLOntologyFormat(RDFFormat.RDFXML),
                 rioOntologyFormat);
         equal(owlapiOntologyPrimer, rioOntologyPrimer);
-        assertEquals(93, rioOntologyPrimer.getAxiomCount());
+        assertEquals(92, rioOntologyPrimer.getAxiomCount());
     }
 
     /*
