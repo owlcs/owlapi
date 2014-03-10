401278c60999cecdd4cd090b78d2a5ff711671a8
Simon Spero <sesuncedu@gmail.com>
2014-03-10 16:57:13 -0400

    Triple Logger will log triples at trace level

--- parsers/src/main/java/org/semanticweb/owlapi/rdf/rdfxml/parser/TripleLogger.java
+++ parsers/src/main/java/org/semanticweb/owlapi/rdf/rdfxml/parser/TripleLogger.java
@@ -1,5 +1,7 @@
 package org.semanticweb.owlapi.rdf.rdfxml.parser;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.semanticweb.owlapi.model.OWLOntologyID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,35 +14,70 @@ import org.slf4j.LoggerFactory;
  */
 public class TripleLogger {
 
-    /** The Constant tripleProcessor. */
-    private static final Logger tripleProcessor = LoggerFactory
+    /** The Constant log. */
+    private static final Logger log = LoggerFactory
             .getLogger(TripleLogger.class);
     // Debug stuff
-    private int count = 0;
+    private AtomicInteger count = new AtomicInteger();
 
     /** @return triples counted */
     public int count() {
-        return count;
+        return count.get();
+    }
+
+    /**
+     * log triples at debug level and increment triple count
+     * 
+     * @param s
+     *        subject
+     * @param p
+     *        predicate
+     * @param o
+     *        object
+     */
+    public void logTriple(Object s, Object p, Object o) {
+        log.trace("s={} p={} o={}", s, p, o);
+        incrementTripleCount();
+    }
+
+    /**
+     * log triples at debug level, including language and datatype, and
+     * increment triple count
+     * 
+     * @param s
+     *        subject
+     * @param p
+     *        predicate
+     * @param o
+     *        object
+     * @param lang
+     *        language
+     * @param datatype
+     *        datatype
+     */
+    public void logTriple(Object s, Object p, Object o, Object lang,
+            Object datatype) {
+        log.trace("s={} p={} o={} l={} dt={}", s, p, o, lang, datatype);
+        incrementTripleCount();
     }
 
     /** increment count and log. */
-    public void incrementTripleCount() {
-        count++;
-        if (count % 10000 == 0) {
-            tripleProcessor.info("Parsed: {} triples", count);
+    private void incrementTripleCount() {
+        if (count.incrementAndGet() % 10000 == 0) {
+            log.debug("Parsed: {} triples", count);
         }
     }
 
     /** log finl count. */
     public void logNumberOfTriples() {
-        tripleProcessor.info("Total number of triples: {}", count);
+        log.debug("Total number of triples: {}", count);
     }
 
     /**
      * @param id
      *        log ontology id
      */
-    public void logOntologyID(OWLOntologyID id) {
-        tripleProcessor.info("Loaded {}", id);
+    synchronized public void logOntologyID(OWLOntologyID id) {
+        log.debug("Loaded {}", id);
     }
 }
