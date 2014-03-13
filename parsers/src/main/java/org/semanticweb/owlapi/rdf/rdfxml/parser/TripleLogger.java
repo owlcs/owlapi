5ba683ef8e3e91d423b7c828ce6f5288368fdaad
Simon Spero <sesuncedu@gmail.com>
2014-03-12 22:18:25 -0400

    Slightly prettier triple logging (using prefixMangler).  Quoting blank nodes, because blank nodes.

--- parsers/src/main/java/org/semanticweb/owlapi/rdf/rdfxml/parser/TripleLogger.java
+++ parsers/src/main/java/org/semanticweb/owlapi/rdf/rdfxml/parser/TripleLogger.java
@@ -1,6 +1,8 @@
 package org.semanticweb.owlapi.rdf.rdfxml.parser;
 
+import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntologyID;
+import org.semanticweb.owlapi.model.PrefixManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,15 +19,17 @@ public class TripleLogger {
     /**
      * The Constant log.
      */
+    private final PrefixManager prefixManager;
     private final Logger log;
     // Debug stuff
     private int count = 0;
 
-    public TripleLogger() {
-        this(LoggerFactory.getLogger(TripleLogger.class));
+    public TripleLogger(PrefixManager prefixManager) {
+        this(LoggerFactory.getLogger(TripleLogger.class), prefixManager);
     }
 
-    public TripleLogger(@Nonnull Logger log) {
+    public TripleLogger(@Nonnull Logger log, PrefixManager prefixManager) {
+        this.prefixManager = prefixManager;
         this.log = log;
     }
 
@@ -36,9 +40,31 @@ public class TripleLogger {
         return count;
     }
 
-    synchronized public void logTriple(Object s, Object p, Object o) {
-        log.trace("s={} p={} o={}", s, p, o);
-        incrementTripleCount();
+    synchronized public void logResourceTriple(String s, String p, String o) {
+        if (log.isTraceEnabled()) {
+            logResourceTriple(IRI.create(s), IRI.create(p), IRI.create(o));
+        }
+    }
+
+    synchronized public void logResourceTriple(IRI s, IRI p, IRI o) {
+        if (log.isTraceEnabled()) {
+            log.trace("{} {} {}.", shorten(s), shorten(p), shorten(o));
+            incrementTripleCount();
+        }
+    }
+
+
+    synchronized public void logLiteralTriple(String s, String p, String o) {
+        if (log.isTraceEnabled()) {
+            logLiteralTriple(IRI.create(s), IRI.create(p), o);
+        }
+    }
+
+    synchronized public void logLiteralTriple(IRI s, IRI p, String o) {
+        if (log.isTraceEnabled()) {
+            log.trace("{} {} \"{}\".", shorten(s), shorten(p), o);
+            incrementTripleCount();
+        }
     }
 
     /**
@@ -64,4 +90,17 @@ public class TripleLogger {
     synchronized public void logOntologyID(OWLOntologyID id) {
         log.debug("Loaded {}", id);
     }
+
+    private String shorten(IRI iri) {
+        if (prefixManager == null) {
+            return iri.toQuotedString();
+        } else {
+            String result = prefixManager.getPrefixIRI(iri);
+            if (result == null) {
+                result = iri.toQuotedString();
+            }
+            return result;
+        }
+    }
+
 }
