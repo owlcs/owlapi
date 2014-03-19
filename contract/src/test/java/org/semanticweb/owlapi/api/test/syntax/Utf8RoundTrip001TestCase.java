874aa60902024a2f295b3a41f32bdb1407eeb637
ignazio <ignazio.palmisano@gmail.com>
2014-03-19 21:40:37 +0000

    Fixed #47 Input with character = 0240 (octal) should fail

--- contract/src/test/java/org/semanticweb/owlapi/api/test/syntax/Utf8RoundTrip001TestCase.java
+++ contract/src/test/java/org/semanticweb/owlapi/api/test/syntax/Utf8RoundTrip001TestCase.java
@@ -48,6 +48,7 @@ import org.semanticweb.owlapi.api.test.Factory;
 import org.semanticweb.owlapi.io.OWLFunctionalSyntaxOntologyFormat;
 import org.semanticweb.owlapi.io.StringDocumentSource;
 import org.semanticweb.owlapi.io.StringDocumentTarget;
+import org.semanticweb.owlapi.io.UnparsableOntologyException;
 import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
@@ -70,6 +71,31 @@ public class Utf8RoundTrip001TestCase {
         ByteArrayInputStream in = new ByteArrayInputStream(onto.getBytes());
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         manager.saveOntology(manager.loadOntologyFromOntologyDocument(in), out);
+    }
+
+    @Test(expected = UnparsableOntologyException.class)
+    public void testInvalidUTF8roundTrip() throws OWLOntologyCreationException {
+        // this test checks for the condition described in issue #47
+        // Input with character = 0240 (octal) should fail parsing but is read
+        // in as an owl/xml file
+        OWLOntologyManager manager = Factory.getManager();
+        String onto = "<!DOCTYPE rdf:RDF [\n"
+                + "<!ENTITY xsd \"http://www.w3.org/2001/XMLSchema#\" >\n"
+                + "]>\n"
+                + "<rdf:RDF \n"
+                + "xml:base=\n"
+                + "\"http://www.example.org/ISA14#\" \n"
+                + "xmlns:owl =\"http://www.w3.org/2002/07/owl#\" \n"
+                + "xmlns:rdf =\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" \n"
+                + "xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\" \n"
+                + "xmlns:xsd =\"http://www.w3.org/2001/XMLSchema#\" \n"
+                + "xmlns:ibs =\"http://www.example.org/ISA14#\" >\n"
+                + "<owl:Ontology rdf:about=\"#\" />\n"
+                + (char) Integer.valueOf("240", 8).intValue()
+                + "<owl:Class rdf:about=\"http://www.example.org/ISA14#Researcher\"/>\n"
+                + "</rdf:RDF>";
+        ByteArrayInputStream in = new ByteArrayInputStream(onto.getBytes());
+        manager.loadOntologyFromOntologyDocument(in);
     }
 
     @Test
