c3dddd908c689e53bcefabbf0c1a0b32c2888231
ignazio <ignazio.palmisano@gmail.com>
2014-03-27 21:47:59 +0000

    Ensure #114 Escaping characters in default OBOParser 3.4.10 is fixed
    This bug is not reproducible in the current code base. The test makes
    sure it stays that way.

---
+++ contract/src/test/java/org/obolibrary/obo2owl/RoundTripXrefescapecolonTest.java
@@ -0,0 +1,34 @@
+package org.obolibrary.obo2owl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.obolibrary.oboformat.model.OBODoc;
+import org.obolibrary.oboformat.parser.OBOFormatParserException;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+@SuppressWarnings("javadoc")
+public class RoundTripXrefescapecolonTest extends RoundTripTest {
+
+    String file = "xref_escapecolon.obo";
+
+    @Test
+    public void testRoundTrip() throws Exception {
+        roundTripOBOFile(file, true);
+    }
+
+    @Test
+    public void shouldContainExpectedAnnotation()
+            throws OBOFormatParserException, IOException,
+            OWLOntologyCreationException {
+        OBODoc oboFile = parseOBOFile(file);
+        OWLOntology o = convert(oboFile);
+        IRI expected = IRI
+                .create("http://purl.obolibrary.org/obo/GO_0042062%3A");
+        assertEquals(18, o.getAnnotationAssertionAxioms(expected).size());
+    }
+}
