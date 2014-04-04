63d993caef7f14d1bcb1f724ea97bfabff57131f
Matthew Horridge <matthew.horridge@stanford.edu>
2014-04-04 21:12:28 +0100

    Fixed some issues with OWL2Datatype.getPrefixedName().  Added test cases for this.  Added hamcrest as a dependency.

--- api/src/main/java/org/semanticweb/owlapi/vocab/OWL2Datatype.java
+++ api/src/main/java/org/semanticweb/owlapi/vocab/OWL2Datatype.java
@@ -47,6 +47,7 @@ import org.semanticweb.owlapi.model.OWLRuntimeException;
  */
 public enum OWL2Datatype implements HasIRI, HasShortForm, HasPrefixedName {
 
+
 //@formatter:off
     /** RDF_XML_LITERAL */          RDF_XML_LITERAL          (RDF,  "XMLLiteral",   Category.CAT_STRING_WITHOUT_LANGUAGE_TAG, false, ".*"), 
     /** RDFS_LITERAL */             RDFS_LITERAL             (RDFS, "Literal",      Category.CAT_UNIVERSAL,                   false, ".*"),
@@ -159,7 +160,7 @@ public enum OWL2Datatype implements HasIRI, HasShortForm, HasPrefixedName {
             boolean finite, String regEx) {
         iri = IRI.create(namespace.toString(), shortName);
         this.shortName = shortName;
-        prefixedName = namespace + ":" + shortName;
+        prefixedName = namespace.getPrefixName() + ":" + shortName;
         this.category = category;
         this.finite = finite;
         if (regEx != null) {
