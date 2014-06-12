6179b00322ad2af11fe5a54687b5bdb1f240f367
ignazio <ignazio.palmisano@gmail.com>
2014-06-12 08:11:42 +0100

    warnings removed

--- contract/src/test/java/org/obolibrary/obo2owl/Owl2OboTest.java
+++ contract/src/test/java/org/obolibrary/obo2owl/Owl2OboTest.java
@@ -1,6 +1,6 @@
 package org.obolibrary.obo2owl;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.obolibrary.oboformat.model.OBODoc;
@@ -26,36 +26,31 @@ public class Owl2OboTest extends OboFormatTestBasics {
         OWLOntology ontology = manager.createOntology(ontologyIRI);
         convert(ontology);
         String ontId = OWLAPIOwl2Obo.getOntologyId(ontology);
-        assertTrue("test".equals(ontId));
+        assertEquals("test", ontId);
         IRI iri = IRI.create("http://purl.obolibrary.org/obo/OBI_0000306");
         String id = OWLAPIOwl2Obo.getIdentifier(iri);
         assertTrue("OBI:0000306".endsWith(id));
-        if (false) {
-            // TODO
-            iri = IRI
-                    .create("http://purl.org/obo/owl/NCBITaxon#NCBITaxon_5794");
-            id = OWLAPIOwl2Obo.getIdentifier(iri);
-            assertTrue("http://purl.org/obo/owl/NCBITaxon#NCBITaxon_5794"
-                    .equals(id));
-        }
+        iri = IRI.create("http://purl.org/obo/owl/NCBITaxon#NCBITaxon_5794");
+        id = OWLAPIOwl2Obo.getIdentifier(iri);
+        assertEquals("NCBITaxon_5794", id);
         iri = IRI.create("http://purl.obolibrary.org/obo/IAO_0000119");
         id = OWLAPIOwl2Obo.getIdentifier(iri);
-        assertTrue("IAO:0000119".equals(id));
+        assertEquals("IAO:0000119", id);
         iri = IRI.create("http://purl.obolibrary.org/obo/caro_part_of");
         id = OWLAPIOwl2Obo.getIdentifier(iri);
-        assertTrue("http://purl.obolibrary.org/obo/caro_part_of".equals(id));
+        assertEquals("http://purl.obolibrary.org/obo/caro_part_of", id);
         iri = IRI.create("http://purl.obolibrary.org/obo/MyOnt#_part_of");
         id = OWLAPIOwl2Obo.getIdentifier(iri);
-        assertTrue("MyOnt:part_of".equals(id));
+        assertEquals("MyOnt:part_of", id);
         iri = IRI.create("http://purl.obolibrary.org/obo/MyOnt#termid");
         id = OWLAPIOwl2Obo.getIdentifier(iri);
-        assertTrue("termid".equals(id));
+        assertEquals("termid", id);
         // unprefixed IDs from different ontology
         iri = IRI.create("http://purl.obolibrary.org/obo/MyOnt#termid");
         id = OWLAPIOwl2Obo.getIdentifier(iri, ontology);
         // assertTrue("http://purl.obolibrary.org/obo/MyOnt#termid".equals(id));
         iri = IRI.create("http://www.w3.org/2002/07/owl#topObjectProperty");
         id = OWLAPIOwl2Obo.getIdentifier(iri);
-        assertTrue("owl:topObjectProperty".equals(id));
+        assertEquals("owl:topObjectProperty", id);
     }
 }
--- contract/src/test/java/org/obolibrary/obo2owl/RoundTripRelationshipVsProperty.java
+++ contract/src/test/java/org/obolibrary/obo2owl/RoundTripRelationshipVsProperty.java
@@ -2,10 +2,11 @@ package org.obolibrary.obo2owl;
 
 import org.junit.Test;
 
+@SuppressWarnings("javadoc")
 public class RoundTripRelationshipVsProperty extends RoundTripTest {
 
-	@Test
-	public void roundTripRelationshipVsProperty() throws Exception {
-		roundTripOBOFile("relationship_vs_property.obo", true);
-	}
+    @Test
+    public void roundTripRelationshipVsProperty() throws Exception {
+        roundTripOBOFile("relationship_vs_property.obo", true);
+    }
 }
