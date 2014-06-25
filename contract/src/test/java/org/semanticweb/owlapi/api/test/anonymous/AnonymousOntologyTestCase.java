12eb7865e2f4a5d34c3ad476add79427f074aa85
ignazio <ignazio.palmisano@gmail.com>
2014-06-25 07:44:13 +0100

    Fix #215 getOntology(OWLOntologyID) can throw a NPE

--- contract/src/test/java/org/semanticweb/owlapi/api/test/anonymous/AnonymousOntologyTestCase.java
+++ contract/src/test/java/org/semanticweb/owlapi/api/test/anonymous/AnonymousOntologyTestCase.java
@@ -1,22 +1,21 @@
 package org.semanticweb.owlapi.api.test.anonymous;
 
+import static org.junit.Assert.assertNull;
+
 import org.junit.Test;
-import org.semanticweb.owlapi.api.test.baseclasses.TestBase;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyID;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
-/**
- * @author ignazio
- */
-public class AnonymousOntologyTestCase extends TestBase {
+@SuppressWarnings("javadoc")
+public class AnonymousOntologyTestCase {
 
     @Test
     public void shouldNotFailOnAnonymousOntologySearch()
             throws OWLOntologyCreationException {
         OWLOntologyManager man = OWLManager.createOWLOntologyManager();
         man.createOntology(new OWLOntologyID());
-        man.getOntology(new OWLOntologyID());
+        assertNull(man.getOntology(new OWLOntologyID()));
     }
 }
--- impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLOntologyManagerImpl.java
+++ impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLOntologyManagerImpl.java
@@ -273,13 +273,28 @@ public class OWLOntologyManagerImpl implements OWLOntologyManager,
         return result;
     }
 
+    /**
+     * @param first
+     *        first id
+     * @param second
+     *        second id
+     * @return true if the ids are equal or have the same ontology IRI. Ontology
+     *         version is ignored.
+     */
+    private static boolean
+            matchingIDs(OWLOntologyID first, OWLOntologyID second) {
+        if (first.isAnonymous() || second.isAnonymous()) {
+            return first.equals(second);
+        }
+        return first.getOntologyIRI().equals(second.getOntologyIRI());
+    }
+
     @Override
     public OWLOntology getOntology(OWLOntologyID ontologyID) {
         OWLOntology result = ontologiesByID.get(ontologyID);
-        if (result == null) {
+        if (result == null && !ontologyID.isAnonymous()) {
             for (OWLOntologyID nextOntologyID : ontologiesByID.keySet()) {
-                if (ontologyID.getOntologyIRI().equals(
-                        nextOntologyID.getOntologyIRI())) {
+                if (matchingIDs(ontologyID, nextOntologyID)) {
                     result = ontologiesByID.get(nextOntologyID);
                 }
             }
