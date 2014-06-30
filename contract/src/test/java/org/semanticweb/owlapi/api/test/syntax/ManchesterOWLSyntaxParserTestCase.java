f0e2f439b370f173a04e49bbcabe5281d79e208a
Peter Ansell <p_ansell@yahoo.com>
2014-06-30 12:21:32 +1000

    Fix/workaround manchester test by using test base equals method

--- contract/src/test/java/org/semanticweb/owlapi/api/test/syntax/ManchesterOWLSyntaxParserTestCase.java
+++ contract/src/test/java/org/semanticweb/owlapi/api/test/syntax/ManchesterOWLSyntaxParserTestCase.java
@@ -400,10 +400,7 @@ public class ManchesterOWLSyntaxParserTestCase extends TestBase {
                 + "AnnotationAssertion(Annotation(<http://x.org/p2> \"foo\") <http://x.org/p> <http://x.org/c> \"v1\"))";
         OWLOntology o = loadOntologyFromString(in);
         OWLOntology result = roundTrip(o, new ManchesterSyntaxDocumentFormat());
-        for (OWLAxiom ax : o.getAxioms()) {
-            assert ax != null;
-            assertTrue(result.containsAxiom(ax));
-        }
+        equal(o, result);
     }
 
     @Test
