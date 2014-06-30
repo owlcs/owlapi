28eaff3787e6118437eb5319c040714038f482fe
hdietze <hdietze@Eaglet-2.local>
2014-06-30 13:31:02 -0700

    bug fix: missing [] for synonyms

--- oboformat/src/main/java/org/obolibrary/oboformat/writer/OBOFormatWriter.java
+++ oboformat/src/main/java/org/obolibrary/oboformat/writer/OBOFormatWriter.java
@@ -508,6 +508,7 @@ public class OBOFormatWriter {
         if (!xrefs.isEmpty()) {
             appendXrefs(sb, xrefs);
         } else if (OboFormatTag.TAG_DEF.getTag().equals(clause.getTag())
+                || OboFormatTag.TAG_SYNONYM.getTag().equals(clause.getTag())
                 || OboFormatTag.TAG_EXPAND_EXPRESSION_TO.getTag().equals(
                         clause.getTag())
                 || OboFormatTag.TAG_EXPAND_ASSERTION_TO.getTag().equals(
