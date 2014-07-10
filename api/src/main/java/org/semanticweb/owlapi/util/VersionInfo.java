422dcca3c47f98b736c1c2cbb2d144843c4a5484
ignazio <ignazio.palmisano@gmail.com>
2014-07-10 21:13:32 +0100

    RC 1

--- api/src/main/java/org/semanticweb/owlapi/util/VersionInfo.java
+++ api/src/main/java/org/semanticweb/owlapi/util/VersionInfo.java
@@ -33,7 +33,7 @@ public class VersionInfo {
         if (v != null) {
             version = v;
         } else {
-            version = "4.0.0-SNAPSHOT";
+            version = "4.0.0-RC1";
         }
     }
 
