5cd231903ae513dc1fc4ea88c8528d3e96367448
ignazio <ignazio.palmisano@gmail.com>
2014-08-03 12:50:37 +0100

    readded getValue as deprecated

--- impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLValueRestrictionImpl.java
+++ impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLValueRestrictionImpl.java
@@ -43,6 +43,11 @@ public abstract class OWLValueRestrictionImpl<V extends OWLObject> extends
     }
 
     @Override
+    public V getValue() {
+        return getFiller();
+    }
+
+    @Override
     public boolean equals(Object obj) {
         if (super.equals(obj)) {
             if (!(obj instanceof OWLHasValueRestriction<?>)) {
