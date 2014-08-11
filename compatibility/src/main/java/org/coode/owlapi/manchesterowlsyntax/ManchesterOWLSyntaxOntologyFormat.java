8fb7a8e890c9496b9593a7e05a76277e2d758f02
ignazio <ignazio.palmisano@gmail.com>
2014-08-11 18:52:40 +0100

    fix #255 PrefixOWLOntologyFormat is missing from the compatibility module
    Fixed as previously for OWLOntologyFormat, added an interface with the
    same name as the class and made the compatibilty formats implement it.
    The interface extends PrefixManager so should cover most of the use
    cases for PrefixOWLOntologyFormat

--- compatibility/src/main/java/org/coode/owlapi/manchesterowlsyntax/ManchesterOWLSyntaxOntologyFormat.java
+++ compatibility/src/main/java/org/coode/owlapi/manchesterowlsyntax/ManchesterOWLSyntaxOntologyFormat.java
@@ -12,7 +12,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. */
 package org.coode.owlapi.manchesterowlsyntax;
 
-import org.semanticweb.owlapi.model.OWLOntologyFormat;
+import org.semanticweb.owlapi.vocab.PrefixOWLOntologyFormat;
 
 /**
  * @author Matthew Horridge, The University Of Manchester, Bio-Health
@@ -23,7 +23,7 @@ import org.semanticweb.owlapi.model.OWLOntologyFormat;
 @Deprecated
 public class ManchesterOWLSyntaxOntologyFormat extends
         org.semanticweb.owlapi.formats.ManchesterSyntaxDocumentFormat implements
-        OWLOntologyFormat {
+        PrefixOWLOntologyFormat {
 
     private static final long serialVersionUID = 30406L;
 }
---
+++ compatibility/src/main/java/org/semanticweb/owlapi/vocab/PrefixOWLOntologyFormat.java
@@ -0,0 +1,13 @@
+package org.semanticweb.owlapi.vocab;
+
+import java.io.Serializable;
+
+import org.semanticweb.owlapi.model.OWLOntologyFormat;
+import org.semanticweb.owlapi.model.PrefixManager;
+
+/**Transition interface, for compatibility purposes. 
+@deprecated use PrefixDocumentFormat instead*/
+@Deprecated
+public interface PrefixOWLOntologyFormat extends OWLOntologyFormat,
+        PrefixManager, Serializable {
+}
