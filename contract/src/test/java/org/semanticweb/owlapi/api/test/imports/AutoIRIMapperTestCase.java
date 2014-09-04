061f2f5cc2891870d9ed83e1910b836f04c1d302
ignazio <ignazio.palmisano@gmail.com>
2014-09-05 00:11:01 +0100

    Fix #278 AutoIRIMapper is not namespace aware

---
+++ contract/src/test/java/org/semanticweb/owlapi/api/test/imports/AutoIRIMapperTestCase.java
@@ -0,0 +1,41 @@
+package org.semanticweb.owlapi.api.test.imports;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import javax.annotation.Nonnull;
+
+import org.junit.Test;
+import org.semanticweb.owlapi.api.test.baseclasses.TestBase;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLRuntimeException;
+import org.semanticweb.owlapi.util.AutoIRIMapper;
+
+@SuppressWarnings("javadoc")
+public class AutoIRIMapperTestCase extends TestBase {
+
+    @Nonnull
+    private static final File RESOURCES;
+    static {
+        File f = new File("contract/src/test/resources/");
+        if (f.exists()) {
+            RESOURCES = f;
+        } else {
+            f = new File("src/test/resources/");
+            if (f.exists()) {
+                RESOURCES = f;
+            } else {
+                throw new OWLRuntimeException(
+                        "ManchesterImportTestCase: NO RESOURCE FOLDER ACCESSIBLE");
+            }
+        }
+    }
+
+    @Test
+    public void shouldTestIRIMapperForOWLXML() {
+        AutoIRIMapper mapper = new AutoIRIMapper(RESOURCES, false);
+        assertTrue(mapper.getDocumentIRI(IRI.create("urn:test:prem"))
+                .toString().endsWith("/urntestontology.xml"));
+    }
+}
