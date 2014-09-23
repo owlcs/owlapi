d4c519416a43097a5a5017b97ca7a2e1a7370e90
ignazio <ignazio.palmisano@gmail.com>
2014-09-23 21:32:23 +0100

    fix #288 Errors with object property assertions with inverseOf properties

--- impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLObjectPropertyAssertionAxiomImpl.java
+++ impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLObjectPropertyAssertionAxiomImpl.java
@@ -95,8 +95,8 @@ public class OWLObjectPropertyAssertionAxiomImpl
         } else {
             OWLObjectInverseOf property = (OWLObjectInverseOf) getProperty();
             OWLObjectPropertyExpression invProp = property.getInverse();
-            return new OWLObjectPropertyAssertionAxiomImpl(getSubject(),
-                    invProp, getObject(), NO_ANNOTATIONS);
+            return new OWLObjectPropertyAssertionAxiomImpl(getObject(),
+                    invProp, getSubject(), NO_ANNOTATIONS);
         }
     }
 
