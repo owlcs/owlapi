3a8236e3d3e3eacd06900c01df06b43ca7a173f4
ignazio <ignazio.palmisano@gmail.com>
2014-10-28 08:12:13 +0000

    AssertionProvider broken into annotation, object, data, class and individual providers.

--- api/src/main/java/org/semanticweb/owlapi/model/OWLDataFactory.java
+++ api/src/main/java/org/semanticweb/owlapi/model/OWLDataFactory.java
@@ -25,19 +25,23 @@ import org.semanticweb.owlapi.model.axiomproviders.PropertyChainAxiomProvider;
 import org.semanticweb.owlapi.model.axiomproviders.PropertyCharacteristicAxiomProvider;
 import org.semanticweb.owlapi.model.axiomproviders.RangeAxiomProvider;
 import org.semanticweb.owlapi.model.axiomproviders.SubAxiomProvider;
+import org.semanticweb.owlapi.model.providers.AnnotationAssertionProvider;
 import org.semanticweb.owlapi.model.providers.AnnotationProvider;
-import org.semanticweb.owlapi.model.providers.AssertionProvider;
 import org.semanticweb.owlapi.model.providers.CardinalityRestrictionProvider;
+import org.semanticweb.owlapi.model.providers.ClassAssertionProvider;
 import org.semanticweb.owlapi.model.providers.ComplementProvider;
+import org.semanticweb.owlapi.model.providers.DataAssertionProvider;
 import org.semanticweb.owlapi.model.providers.DatatypeExpressionProvider;
 import org.semanticweb.owlapi.model.providers.ExistentialProvider;
 import org.semanticweb.owlapi.model.providers.HasSelfProvider;
 import org.semanticweb.owlapi.model.providers.HasValueProvider;
+import org.semanticweb.owlapi.model.providers.IndividualAssertionProvider;
 import org.semanticweb.owlapi.model.providers.IntersectionProvider;
 import org.semanticweb.owlapi.model.providers.InverseProvider;
 import org.semanticweb.owlapi.model.providers.LiteralProvider;
 import org.semanticweb.owlapi.model.providers.NominalProvider;
 import org.semanticweb.owlapi.model.providers.OWLVocabularyProvider;
+import org.semanticweb.owlapi.model.providers.ObjectAssertionProvider;
 import org.semanticweb.owlapi.model.providers.UnionProvider;
 import org.semanticweb.owlapi.model.providers.UniversalProvider;
 
@@ -51,15 +55,17 @@ import org.semanticweb.owlapi.model.providers.UniversalProvider;
 public interface OWLDataFactory extends SWRLDataFactory, OWLEntityProvider,
         OWLEntityByTypeProvider, OWLAnonymousIndividualProvider,
         OWLAnonymousIndividualByIdProvider, OWLVocabularyProvider,
-        LiteralProvider, AnnotationProvider, AssertionProvider,
-        CardinalityRestrictionProvider, DisjointAxiomProvider,
-        EquivalentAxiomProvider, PropertyCharacteristicAxiomProvider,
-        DatatypeExpressionProvider, DomainAxiomProvider, RangeAxiomProvider,
-        IntersectionProvider, UnionProvider, SubAxiomProvider,
-        DeclarationAxiomProvider, ComplementProvider, NominalProvider,
-        UniversalProvider, ExistentialProvider, HasKeyAxiomProvider,
-        InverseAxiomProvider, HasValueProvider, InverseProvider,
-        HasSelfProvider, DisjointUnionAxiomProvider, PropertyChainAxiomProvider {
+        LiteralProvider, AnnotationProvider, AnnotationAssertionProvider,
+        ClassAssertionProvider, DataAssertionProvider, ObjectAssertionProvider,
+        IndividualAssertionProvider, CardinalityRestrictionProvider,
+        DisjointAxiomProvider, EquivalentAxiomProvider,
+        PropertyCharacteristicAxiomProvider, DatatypeExpressionProvider,
+        DomainAxiomProvider, RangeAxiomProvider, IntersectionProvider,
+        UnionProvider, SubAxiomProvider, DeclarationAxiomProvider,
+        ComplementProvider, NominalProvider, UniversalProvider,
+        ExistentialProvider, HasKeyAxiomProvider, InverseAxiomProvider,
+        HasValueProvider, InverseProvider, HasSelfProvider,
+        DisjointUnionAxiomProvider, PropertyChainAxiomProvider {
 
     /**
      * @param importedOntologyIRI
---
+++ api/src/main/java/org/semanticweb/owlapi/model/providers/IndividualAssertionProvider.java
@@ -0,0 +1,108 @@
+/* This file is part of the OWL API.
+ * The contents of this file are subject to the LGPL License, Version 3.0.
+ * Copyright 2014, The University of Manchester
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * Alternatively, the contents of this file may be used under the terms of the Apache License, Version 2.0 in which case, the provisions of the Apache License Version 2.0 are applicable instead of those above.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. */
+package org.semanticweb.owlapi.model.providers;
+
+import static org.semanticweb.owlapi.util.OWLAPIPreconditions.checkIterableNotNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLDifferentIndividualsAxiom;
+import org.semanticweb.owlapi.model.OWLIndividual;
+import org.semanticweb.owlapi.model.OWLSameIndividualAxiom;
+import org.semanticweb.owlapi.util.CollectionFactory;
+
+/** Individual (sameAs and differentFrom) assertion provider. */
+public interface IndividualAssertionProvider extends LiteralProvider {
+
+    /**
+     * @param individuals
+     *        Cannot be null or contain nulls.
+     * @return a same individuals axiom with specified individuals
+     */
+    @Nonnull
+    default OWLSameIndividualAxiom getOWLSameIndividualAxiom(
+            @Nonnull Set<? extends OWLIndividual> individuals) {
+        return getOWLSameIndividualAxiom(individuals, Collections.emptySet());
+    }
+
+    /**
+     * @param individual
+     *        individual
+     * @return a same individuals axiom with specified individuals
+     */
+    @Nonnull
+    default OWLSameIndividualAxiom getOWLSameIndividualAxiom(
+            @Nonnull OWLIndividual... individual) {
+        checkIterableNotNull(individual, "individuals cannot be null", true);
+        Set<OWLIndividual> inds = new HashSet<>();
+        inds.addAll(Arrays.asList(individual));
+        return getOWLSameIndividualAxiom(inds);
+    }
+
+    /**
+     * @param individuals
+     *        Cannot be null or contain nulls.
+     * @param annotations
+     *        A set of annotations. Cannot be null or contain nulls.
+     * @return a same individuals axiom with specified individuals and
+     *         annotations
+     */
+    @Nonnull
+    OWLSameIndividualAxiom getOWLSameIndividualAxiom(
+            @Nonnull Set<? extends OWLIndividual> individuals,
+            @Nonnull Set<? extends OWLAnnotation> annotations);
+
+    /**
+     * @param individuals
+     *        Cannot be null or contain nulls.
+     * @return a different individuals axiom with specified individuals
+     */
+    @Nonnull
+    default OWLDifferentIndividualsAxiom getOWLDifferentIndividualsAxiom(
+            @Nonnull Set<? extends OWLIndividual> individuals) {
+        return getOWLDifferentIndividualsAxiom(individuals,
+                Collections.emptySet());
+    }
+
+    /**
+     * @param individuals
+     *        Cannot be null or contain nulls.
+     * @return a different individuals axiom with specified individuals
+     */
+    @Nonnull
+    default OWLDifferentIndividualsAxiom getOWLDifferentIndividualsAxiom(
+            @Nonnull OWLIndividual... individuals) {
+        checkIterableNotNull(individuals, "individuals cannot be null", true);
+        return getOWLDifferentIndividualsAxiom(CollectionFactory
+                .createSet(individuals));
+    }
+
+    /**
+     * @param individuals
+     *        Cannot be null or contain nulls.
+     * @param annotations
+     *        A set of annotations. Cannot be null or contain nulls.
+     * @return a different individuals axiom with specified individuals and
+     *         annotations
+     */
+    @Nonnull
+    OWLDifferentIndividualsAxiom getOWLDifferentIndividualsAxiom(
+            @Nonnull Set<? extends OWLIndividual> individuals,
+            @Nonnull Set<? extends OWLAnnotation> annotations);
+}
