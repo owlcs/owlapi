6eabd0374bf9eb999baf26a824e048b0b6d6aa04
Simon Spero <sesuncedu@gmail.com>
2014-11-26 15:18:09 -0500

    Use compressed taxon in benchmark.

--- benchmarks/src/main/java/net/sourceforge/owlapi/MyBenchmark.java
+++ benchmarks/src/main/java/net/sourceforge/owlapi/MyBenchmark.java
@@ -26,39 +26,60 @@
 package net.sourceforge.owlapi;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.FileDocumentSource;
 import org.semanticweb.owlapi.io.OWLOntologyDocumentSource;
-import org.semanticweb.owlapi.io.OWLOntologyLoaderMetaData;
-import org.semanticweb.owlapi.io.RDFParserMetaData;
-import org.semanticweb.owlapi.io.RDFTriple;
-import org.semanticweb.owlapi.io.StreamDocumentSource;
-import org.semanticweb.owlapi.model.OWLDocumentFormat;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyLoaderConfiguration;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import uk.ac.manchester.cs.owl.owlapi.OWLOntologyImpl;
 
-import java.util.Set;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
 
+@State(Scope.Thread)
 public class MyBenchmark {
 
+    private static File uncompressedTaxonFile;
+
+    @Setup(Level.Trial)
+    public void setUp() throws IOException {
+        uncompressedTaxonFile = File.createTempFile("taxons", "ofn");
+        InputStream resourceAsStream = getClass().getResourceAsStream("/ncbitaxon.rdf.ofn.gz");
+        try (GZIPInputStream in = new GZIPInputStream(resourceAsStream);
+             FileOutputStream out = new FileOutputStream(uncompressedTaxonFile)) {
+            int n;
+            byte buf[] = new byte[8192];
+            while ((n = in.read(buf)) >= 0) {
+                out.write(buf, 0, n);
+            }
+            out.flush();
+        }
+    }
+
+    @TearDown(Level.Trial)
+    public void tearDown() {
+        uncompressedTaxonFile.delete();
+    }
+
+    @BenchmarkMode(Mode.AverageTime)
     @Benchmark
     public void testLoadTaxonFSS() throws OWLOntologyCreationException {
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        OWLOntologyDocumentSource ds = new StreamDocumentSource(getClass().getResourceAsStream("/ncbitaxon.rdf.ofn") );
+        OWLOntologyDocumentSource ds = new FileDocumentSource(uncompressedTaxonFile);
         OWLOntologyLoaderConfiguration config = new OWLOntologyLoaderConfiguration().setStrict(false);
 
         OWLOntologyImpl ontology = (OWLOntologyImpl) manager.loadOntologyFromOntologyDocument(ds, config);
-        OWLDocumentFormat ontologyFormat = manager.getOntologyFormat(ontology);
-        OWLOntologyLoaderMetaData ontologyLoaderMetaData = ontologyFormat.getOntologyLoaderMetaData();
-        if (ontologyLoaderMetaData instanceof RDFParserMetaData) {
-            RDFParserMetaData rdfParserMetaData = (RDFParserMetaData) ontologyLoaderMetaData;
-            Set<RDFTriple> unparsedTriples = rdfParserMetaData.getUnparsedTriples();
-            if (unparsedTriples != null) {
-                System.err.printf("There were %,d unparsed triples", unparsedTriples.size());
-            }
-        }
-        ontology.trimToSize();
         manager.removeOntology(ontology);
     }
 
