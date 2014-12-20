1d62d934dfaa31a29686174693b2982ea941d04f
Simon Spero <sesuncedu@gmail.com>
2014-12-19 19:04:26 -0500

    owlapi-321 Generics related compilation issue in owlaxiomimpl

--- impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLAxiomImplWithoutEntityAndAnonCaching.java
+++ impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLAxiomImplWithoutEntityAndAnonCaching.java
@@ -61,8 +61,8 @@ public abstract class OWLAxiomImplWithoutEntityAndAnonCaching extends
         checkNotNull(annotations, "annotations cannot be null");
         if (!annotations.isEmpty()) {
             if (annotations.size() == 1) {
-                this.annotations = Collections.singletonList(annotations
-                        .iterator().next());
+                OWLAnnotation tmp = annotations.iterator().next();
+                this.annotations = Collections.singletonList(tmp);
             } else {
                 this.annotations = (List<OWLAnnotation>) sortOptionally(annotations);
             }
