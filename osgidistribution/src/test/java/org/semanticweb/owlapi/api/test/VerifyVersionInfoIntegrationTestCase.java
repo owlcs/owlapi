bddbb60c9ca12707111d8dd96a7d7a5e95831a41
ignazio <ignazio.palmisano@gmail.com>
2015-03-08 01:22:26 +0000

    move IntegrationTest and all integration tests to osgidistribution
    Simpler structure for integration tests.

--- osgidistribution/src/test/java/org/semanticweb/owlapi/api/test/VerifyVersionInfoIntegrationTestCase.java
+++ osgidistribution/src/test/java/org/semanticweb/owlapi/api/test/VerifyVersionInfoIntegrationTestCase.java
@@ -12,8 +12,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. */
 package org.semanticweb.owlapi.api.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.File;
 
@@ -30,7 +29,7 @@ import org.w3c.dom.NodeList;
 
 @SuppressWarnings("javadoc")
 @Category(IntegrationTest.class)
-public class VerifyVersionInfoTestCase {
+public class VerifyVersionInfoIntegrationTestCase {
 
     @Test
     public void checkMatchVersion() throws Exception {
