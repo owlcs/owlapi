2fb39fe7d0cfac13f46668568ac50876146dc65f
ignazio <ignazio.palmisano@gmail.com>
2015-03-11 22:42:39 +0000

    fix integration tests execution

--- osgidistribution/src/test/java/org/semanticweb/owlapi/BundleIsLoadableIntegrationTestCase.java
+++ osgidistribution/src/test/java/org/semanticweb/owlapi/BundleIsLoadableIntegrationTestCase.java
@@ -1,7 +1,9 @@
 package org.semanticweb.owlapi;
+
 /**
  * Created by ses on 3/5/15.
  */
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -9,8 +11,8 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.HashMap;
+
 import org.apache.felix.framework.FrameworkFactory;
-import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.osgi.framework.Bundle;
@@ -23,35 +25,38 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Category(IntegrationTest.class)
-public class BundleIsLoadableTest {
+public class BundleIsLoadableIntegrationTestCase {
+
     @SuppressWarnings("UnusedDeclaration")
-    private static Logger logger = LoggerFactory.getLogger(BundleIsLoadableTest.class);
+    private static Logger logger = LoggerFactory
+            .getLogger(BundleIsLoadableIntegrationTestCase.class);
 
     @Test
-    public void startBundle() throws MalformedURLException, BundleException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public void startBundle() throws MalformedURLException, BundleException,
+            ClassNotFoundException, IllegalAccessException,
+            InstantiationException {
         HashMap configuration = new HashMap();
-        configuration.put("org.osgi.framework.storage.clean","onFirstInit");
+        configuration.put("org.osgi.framework.storage.clean", "onFirstInit");
         configuration.put("felix.log.level", "4");
         String path = new File("felix-cache").getAbsolutePath();
         configuration.put("org.osgi.framework.storage", path);
         FrameworkFactory frameworkFactory = new FrameworkFactory();
         Framework framework = frameworkFactory.newFramework(configuration);
-        assertNotEquals("framework state", framework.getState(), Framework.ACTIVE);
+        assertNotEquals("framework state", framework.getState(), Bundle.ACTIVE);
         framework.start();
-        assertEquals("framework state", framework.getState(), Framework.ACTIVE);
+        assertEquals("framework state", framework.getState(), Bundle.ACTIVE);
         File dir = new File("target/");
         dir = dir.getAbsoluteFile();
-        File file=null;
-
+        File file = null;
         File[] files = dir.listFiles();
         for (File f : files) {
             String fileName = f.getAbsolutePath();
-            if (fileName.endsWith("jar") && !fileName.contains("sources") && !fileName.contains("javadoc")) {
+            if (fileName.endsWith("jar") && !fileName.contains("sources")
+                    && !fileName.contains("javadoc")) {
                 file = f;
                 break;
             }
         }
-
         assertNotNull("file is null", file);
         URI uri = file.toURI();
         assertNotNull("uri is null", uri);
@@ -59,26 +64,32 @@ public class BundleIsLoadableTest {
         assertNotNull("context is null", context);
         try {
             URL simpleLoggerURL;
-            Bundle simpleLoggerBundle = context.installBundle(getJarURL("slf4j-simple").toString());
+            Bundle simpleLoggerBundle = context.installBundle(getJarURL(
+                    "slf4j-simple").toString());
             System.out.println(simpleLoggerBundle);
-
             simpleLoggerURL = getJarURL("slf4j-api");
-            simpleLoggerBundle = context.installBundle(getJarURL("slf4j-api").toString());
+            simpleLoggerBundle = context.installBundle(getJarURL("slf4j-api")
+                    .toString());
             simpleLoggerBundle.start();
         } catch (Throwable e) {
-            logger.info("Can't install simple logger;", e); //To change body of catch statement use File | Settings | File Templates.
+            logger.info("Can't install simple logger;", e); // To change body of
+                                                            // catch statement
+                                                            // use File |
+                                                            // Settings | File
+                                                            // Templates.
         }
         Bundle bundle = context.installBundle(uri.toString());
         assertNotNull(bundle);
         bundle.start();
         assertEquals("bundle state", bundle.getState(), Bundle.ACTIVE);
-
         Class owlManagerClass = null;
-            owlManagerClass = bundle.loadClass("org.semanticweb.owlapi.apibinding.OWLManager");
+        owlManagerClass = bundle
+                .loadClass("org.semanticweb.owlapi.apibinding.OWLManager");
         assertNotNull("no class owlmanager", owlManagerClass);
         Object o = owlManagerClass.newInstance();
-
-        assertNotEquals("OWLManager class from bundle class loader  equals OWLManager class from system class path", OWLManager.class, owlManagerClass);
+        assertNotEquals(
+                "OWLManager class from bundle class loader  equals OWLManager class from system class path",
+                OWLManager.class, owlManagerClass);
     }
 
     private URL getJarURL(String jarNameFragment) {
@@ -87,12 +98,11 @@ public class BundleIsLoadableTest {
         if (classLoader instanceof URLClassLoader) {
             URLClassLoader urlClassLoader = (URLClassLoader) classLoader;
             for (URL url : urlClassLoader.getURLs()) {
-                if(url.toString().contains(jarNameFragment)) {
+                if (url.toString().contains(jarNameFragment)) {
                     simpleLoggerURL = url;
                     break;
                 }
             }
-
         }
         return simpleLoggerURL;
     }
---
+++ osgidistribution/src/test/java/org/semanticweb/owlapi/api/test/VerifyVersionInfoIntegrationTestCase.java
@@ -0,0 +1,21 @@
+package org.semanticweb.owlapi.api.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.semanticweb.owlapi.test.IntegrationTest;
+import org.semanticweb.owlapi.util.VersionInfo;
+
+@SuppressWarnings("javadoc")
+@Category(IntegrationTest.class)
+public class VerifyVersionInfoIntegrationTestCase {
+
+    @Test
+    public void checkMatchVersion() {
+        // This test will fail after setting the release version if the fallback
+        // has not been updated.
+        VersionInfo info = VersionInfo.getVersionInfo();
+        assertEquals("3.5.3-SNAPSHOT", info.getVersion());
+    }
+}
