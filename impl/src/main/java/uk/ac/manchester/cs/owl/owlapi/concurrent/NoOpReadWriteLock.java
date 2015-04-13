807cde8a589c2f5855959556df23a82a69fd1566
Matthew Horridge <matthew.horridge@stanford.edu>
2015-04-13 10:52:06 -0700

    Added a NoOp implementation of ReadWriteLock

---
+++ impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/concurrent/NoOpReadWriteLock.java
@@ -0,0 +1,27 @@
+package uk.ac.manchester.cs.owl.owlapi.concurrent;
+
+import javax.annotation.Nonnull;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 13/04/15
+ */
+public class NoOpReadWriteLock implements ReadWriteLock {
+
+    private static final NoOpLock NO_OP_LOCK = new NoOpLock();
+
+    @Override
+    @Nonnull
+    public Lock readLock() {
+        return NO_OP_LOCK;
+    }
+
+    @Override
+    @Nonnull
+    public Lock writeLock() {
+        return NO_OP_LOCK;
+    }
+}
---
+++ impl/src/test/java/uk/ac/manchester/cs/owl/owlapi/concurrent/NoOpReadWriteLock_TestCase.java
@@ -0,0 +1,34 @@
+package uk.ac.manchester.cs.owl.owlapi.concurrent;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 13/04/15
+ */
+public class NoOpReadWriteLock_TestCase {
+
+    private NoOpReadWriteLock lock;
+
+    @Before
+    public void setUp() throws Exception {
+        lock = new NoOpReadWriteLock();
+    }
+
+    @Test
+    public void shouldNotReturnNullForReadLock() {
+        assertThat(lock.readLock(), is(not(nullValue())));
+    }
+
+    @Test
+    public void shouldNotReturnNullForWriteLock() {
+        assertThat(lock.writeLock(), is(not(nullValue())));
+    }
+}
