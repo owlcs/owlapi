566180bfef9b1bbc599600e6c6b99a15176cf4e2
ignazio <ignazio.palmisano@gmail.com>
2015-05-23 12:38:10 +0100

    Hopefully remove the cause of random test failures for profiles

--- contract/src/test/java/org/semanticweb/owlapi/profiles/ProfileBase.java
+++ contract/src/test/java/org/semanticweb/owlapi/profiles/ProfileBase.java
@@ -3,39 +3,25 @@ package org.semanticweb.owlapi.profiles;
 import static org.junit.Assert.*;
 import static org.semanticweb.owlapi.profiles.Profiles.*;
 
-import org.junit.BeforeClass;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.io.StringDocumentSource;
+import org.semanticweb.owlapi.api.test.baseclasses.TestBase;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLRuntimeException;
 
 @SuppressWarnings("javadoc")
-public class ProfileBase {
+public class ProfileBase extends TestBase {
 
-    private static OWLOntologyManager m;
-
-    @BeforeClass
-    public static void setupManager() {
-        m = OWLManager.createOWLOntologyManager();
-    }
-
-    OWLOntology o(String in) {
+    protected void test(String in, boolean el, boolean ql, boolean rl, boolean dl) {
+        OWLOntology o;
         try {
-            return m.loadOntologyFromOntologyDocument(new StringDocumentSource(in));
+            o = loadOntologyFromString(in);
+            assertTrue("empty ontology", o.axioms().count() > 0);
+            assertEquals(el, OWL2_EL.checkOntology(o).isInProfile());
+            assertEquals(ql, OWL2_QL.checkOntology(o).isInProfile());
+            assertEquals(rl, OWL2_RL.checkOntology(o).isInProfile());
+            assertEquals(dl, OWL2_DL.checkOntology(o).isInProfile());
         } catch (OWLOntologyCreationException e) {
             throw new OWLRuntimeException(e);
         }
-    }
-
-    protected void test(String in, boolean el, boolean ql, boolean rl, boolean dl) {
-        OWLOntology o = o(in);
-        assertTrue("empty ontology", o.axioms().count() > 0);
-        assertEquals(el, OWL2_EL.checkOntology(o).isInProfile());
-        assertEquals(ql, OWL2_QL.checkOntology(o).isInProfile());
-        assertEquals(rl, OWL2_RL.checkOntology(o).isInProfile());
-        assertEquals(dl, OWL2_DL.checkOntology(o).isInProfile());
-        m.removeOntology(o);
     }
 }
