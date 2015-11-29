a1a6525322d82f6d45e1b1d5a1cbc6756a36b3c3
ignazio <ignazio.palmisano@gmail.com>
2015-11-29 16:20:23 +0000

    Fix for HiddenGCICount.isMetricInvalidated(...) is broken #460

--- tools/src/main/java/org/semanticweb/owlapi/metrics/HiddenGCICount.java
+++ tools/src/main/java/org/semanticweb/owlapi/metrics/HiddenGCICount.java
@@ -12,17 +12,12 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. */
 package org.semanticweb.owlapi.metrics;
 
-import static org.semanticweb.owlapi.util.OWLAPIStreamUtils.asList;
+import static org.semanticweb.owlapi.search.EntitySearcher.*;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.function.Predicate;
 
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyChange;
-import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+import org.semanticweb.owlapi.model.*;
 
 /**
  * Counts the number of "hidden" GCIs in an ontology imports closure. A GCI is
@@ -52,42 +47,22 @@ public class HiddenGCICount extends IntegerValuedMetric {
 
     @Override
     protected boolean isMetricInvalidated(List<? extends OWLOntologyChange> changes) {
-        for (OWLOntologyChange chg : changes) {
-            if (chg.isAxiomChange() && chg.getAxiom() instanceof OWLEquivalentClassesAxiom
-                    || chg.getAxiom() instanceof OWLSubClassOfAxiom) {
-                return true;
-            }
-        }
-        return false;
+        return changes.stream()
+            .filter(OWLOntologyChange::isAxiomChange)
+            .map(c -> c.getAxiom())
+            .anyMatch(equivalentOrSubclass);
     }
+
+    Predicate<OWLAxiom> equivalentOrSubclass = ax -> ax instanceof OWLEquivalentClassesAxiom
+        || ax instanceof OWLSubClassOfAxiom;
+    Predicate<OWLClass> hasEquivalent = c -> getEquivalentClasses(c, getOntologies()).count() > 0;
+    Predicate<OWLClass> isSubclass = c -> getSubClasses(c, getOntologies()).count() > 0;
 
     @Override
     protected Integer recomputeMetric() {
-        Set<OWLClass> processed = new HashSet<>();
-        Set<OWLClass> result = new HashSet<>();
-        for (OWLOntology ont : asList(getOntologies())) {
-            for (OWLClass cls : asList(ont.classesInSignature())) {
-                if (!processed.contains(cls)) {
-                    processed.add(cls);
-                } else {
-                    boolean foundEquivalentClassesAxiom = false;
-                    boolean foundSubClassAxiom = false;
-                    for (OWLOntology o : asList(getOntologies())) {
-                        if (!foundEquivalentClassesAxiom) {
-                            foundEquivalentClassesAxiom = o.equivalentClassesAxioms(cls).count() > 0;
-                        }
-                        if (!foundSubClassAxiom) {
-                            foundSubClassAxiom = o.subClassAxiomsForSubClass(cls).count() > 0;
-                        }
-                        if (foundSubClassAxiom && foundEquivalentClassesAxiom) {
-                            result.add(cls);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        return result.size();
+        return (int) getOntologies().flatMap(o -> o.classesInSignature()).distinct()
+            .filter(hasEquivalent)
+            .filter(isSubclass).count();
     }
 
     @Override
