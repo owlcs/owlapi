0e1711ffc07f5d88da0458c8856f9c4d2dce0de2
Kevin SEJOURNE <Kevin.SEJOURNE@intrinsec.com>
2016-04-11 14:25:01 +0200

    Fix indentation

--- api/src/main/java/org/semanticweb/owlapi/model/SWRLRule.java
+++ api/src/main/java/org/semanticweb/owlapi/model/SWRLRule.java
@@ -1,7 +1,7 @@
 /* This file is part of the OWL API.
  * The contents of this file are subject to the LGPL License, Version 3.0.
  * Copyright 2014, The University of Manchester
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
@@ -21,7 +21,7 @@ import java.util.stream.Stream;
 /**
  * Represent a rule. A rule consists of a head and a body. Both the head and the
  * body consist of a conjunction of atoms.
- *
+ * 
  * @author Matthew Horridge, The University Of Manchester, Medical Informatics
  *         Group
  * @since 2.0.0
@@ -50,7 +50,7 @@ public interface SWRLRule extends OWLLogicalAxiom, SWRLObject {
 
     /**
      * Gets the atoms in the body of the rule.
-     *
+     * 
      * @return A set of {@code SWRLAtom}s, which represent the atoms in the body
      *         of the rule.
      * @deprecated use {@link #body()}
@@ -62,7 +62,7 @@ public interface SWRLRule extends OWLLogicalAxiom, SWRLObject {
 
     /**
      * Gets the atoms in the body of the rule.
-     *
+     * 
      * @return A set of {@code SWRLAtom}s, which represent the atoms in the body
      *         of the rule.
      */
@@ -70,7 +70,7 @@ public interface SWRLRule extends OWLLogicalAxiom, SWRLObject {
 
     /**
      * Gets the atoms in the head of the rule.
-     *
+     * 
      * @return A set of {@code SWRLAtom}s, which represent the atoms in the head
      *         of the rule
      * @deprecated use {@link #head()}
@@ -82,7 +82,7 @@ public interface SWRLRule extends OWLLogicalAxiom, SWRLObject {
 
     /**
      * Gets the atoms in the head of the rule.
-     *
+     * 
      * @return A set of {@code SWRLAtom}s, which represent the atoms in the head
      *         of the rule
      */
@@ -93,7 +93,7 @@ public interface SWRLRule extends OWLLogicalAxiom, SWRLObject {
      * properties, then this method creates and returns a rule where the
      * arguments of these atoms are fliped over and the predicate is the inverse
      * (simplified) property.
-     *
+     * 
      * @return The rule such that any atoms of the form inverseOf(p)(x, y) are
      *         transformed to p(x, y).
      */
@@ -112,14 +112,14 @@ public interface SWRLRule extends OWLLogicalAxiom, SWRLObject {
 
     /**
      * Gets the variables that appear in this rule.
-     *
+     * 
      * @return A set of variables.
      */
     Stream<SWRLVariable> variables();
 
     /**
      * Determines if this rule uses anonymous class expressions in class atoms.
-     *
+     * 
      * @return {@code true} if this rule contains anonymous class expression in
      *         class atoms, otherwise {@code false}.
      */
@@ -127,7 +127,7 @@ public interface SWRLRule extends OWLLogicalAxiom, SWRLObject {
 
     /**
      * Gets the predicates of class atoms.
-     *
+     * 
      * @return A set of class expressions that represent the class class
      *         expressions that are predicates of class atoms.
      * @deprecated use {@link #classAtomPredicates()}
@@ -139,7 +139,7 @@ public interface SWRLRule extends OWLLogicalAxiom, SWRLObject {
 
     /**
      * Gets the predicates of class atoms.
-     *
+     * 
      * @return A set of class expressions that represent the class class
      *         expressions that are predicates of class atoms.
      */
--- impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLObjectImpl.java
+++ impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLObjectImpl.java
@@ -1,7 +1,7 @@
 /* This file is part of the OWL API.
  * The contents of this file are subject to the LGPL License, Version 3.0.
  * Copyright 2014, The University of Manchester
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
@@ -55,7 +55,7 @@ public abstract class OWLObjectImpl implements OWLObject, Serializable, HasIncre
     /**
      * Override point to change hashing strategy if needed - only used in
      * OWLLiteral implementations at the moment.
-     *
+     * 
      * @param object
      *        the object to compute the hashcode for
      * @return the hashcode
@@ -67,7 +67,7 @@ public abstract class OWLObjectImpl implements OWLObject, Serializable, HasIncre
     /**
      * Streams from components need a start point and the order of the
      * components is important.
-     *
+     * 
      * @param start
      *        start index
      * @param s
@@ -160,10 +160,12 @@ public abstract class OWLObjectImpl implements OWLObject, Serializable, HasIncre
         if (typeIndex() != other.typeIndex()) {
             return false;
         }
-        if (this instanceof SWRLRule)// Casts allow because index are the sames
-        	return ((SWRLRule) this).equalsRules((SWRLRule)other);
-        else
-        	return equalStreams(components(), other.components());
+        if (this instanceof SWRLRule) {// Casts allow because index are the sames
+            return ((SWRLRule) this).equalsRules((SWRLRule)other);
+        }
+        else {
+            return equalStreams(components(), other.components());
+        }
     }
 
     @Override
