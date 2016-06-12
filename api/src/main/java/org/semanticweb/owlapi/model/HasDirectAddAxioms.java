568c05cdc3d6eee16d29843d6309e9d41af84c3a
galigator <sejourne_kevin@yahoo.fr>
2016-06-12 15:55:07 +0200

    Add addAxiom method for stream support.

--- api/src/main/java/org/semanticweb/owlapi/model/HasDirectAddAxioms.java
+++ api/src/main/java/org/semanticweb/owlapi/model/HasDirectAddAxioms.java
@@ -13,6 +13,7 @@
 package org.semanticweb.owlapi.model;
 
 import java.util.Collection;
+import java.util.stream.Stream;
 
 import org.semanticweb.owlapi.model.parameters.ChangeApplied;
 
@@ -60,6 +61,17 @@ public interface HasDirectAddAxioms {
     default ChangeApplied add(Collection<? extends OWLAxiom> axioms) {
         return addAxioms(axioms);
     }
+
+    /**
+     * A convenience method that adds a set of axioms to an ontology. The
+     * appropriate AddAxiom change objects are automatically generated.
+     * 
+     * @param axioms
+     *        The axioms to be added. Not {@code null}.
+     * @return ChangeApplied.SUCCESSFULLY if the axiom is added,
+     *         ChangeApplied.UNSUCCESSFULLY otherwise.
+     */
+    ChangeApplied addAxioms(Stream<? extends OWLAxiom> axioms);
 
     /**
      * A convenience method that adds a set of axioms to an ontology. The
