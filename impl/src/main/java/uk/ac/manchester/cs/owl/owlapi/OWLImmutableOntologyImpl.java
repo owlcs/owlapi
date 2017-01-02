d4d1ea2ff504e35be43be8f91010cf0541f89154
ignazio <ignazio.palmisano@gmail.com>
2017-01-02 08:51:26 +0000

    Allow supplier for null error messages
    Existing API forces error message generation before check - this can be
    costly.

--- impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLImmutableOntologyImpl.java
+++ impl/src/main/java/uk/ac/manchester/cs/owl/owlapi/OWLImmutableOntologyImpl.java
@@ -73,7 +73,7 @@ public class OWLImmutableOntologyImpl extends OWLAxiomIndexImpl implements OWLOn
 
     @Override
     public OWLOntologyManager getOWLOntologyManager() {
-        return verifyNotNull(manager, "Manager on ontology " + getOntologyID()
+        return verifyNotNull(manager, () -> "Manager on ontology " + getOntologyID()
             + " is null; the ontology is no longer associated to a manager. Ensure the ontology is not being used after being removed from its manager.");
     }
 
