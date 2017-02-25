6c7c1707885158ed3ce2061ecdce6c1be8639378
Simon Spero <sesuncedu@gmail.com>
2017-02-25 09:32:36 -0500

    Optimize Imports for benchmarks

--- benchmarks/src/main/java/org/semanticweb/owlapi/benchmarks/GazetteerMemoryBenchmark.java
+++ benchmarks/src/main/java/org/semanticweb/owlapi/benchmarks/GazetteerMemoryBenchmark.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.Date;
-
 import org.obolibrary.oboformat.parser.OBOFormatParser;
 
 @SuppressWarnings("javadoc")
