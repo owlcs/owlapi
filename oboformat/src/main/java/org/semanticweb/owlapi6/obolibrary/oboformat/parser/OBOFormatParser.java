7fb28e00db252a06344f4dcd73a6397226bbc282
ignazio <ignazio.palmisano@gmail.com>
2019-03-13 21:14:48 +0000

    Fix build

--- oboformat/src/main/java/org/semanticweb/owlapi6/obolibrary/oboformat/parser/OBOFormatParser.java
+++ oboformat/src/main/java/org/semanticweb/owlapi6/obolibrary/oboformat/parser/OBOFormatParser.java
@@ -151,8 +151,7 @@ public class OBOFormatParser {
     protected OBOFormatParser(MyStream s, Map<String, OBODoc> importsMap) {
         stream = s;
         importCache.putAll(importsMap);
-        Caffeine<String, String> builder = Caffeine.newBuilder()
-            .maximumSize(ConfigurationOptions.CACHE_SIZE.getValue(Integer.class, Collections.emptyMap()).longValue()).maximumWeight(8388608)
+        Caffeine<String, String> builder = Caffeine.newBuilder().maximumWeight(8388608)
             .weigher((String key, String value) -> key.length());
         if (LOG.isDebugEnabled()) {
             builder.recordStats();
