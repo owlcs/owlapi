0895471d343890a65d851109469a58778dad9291
ignazio <ignazio.palmisano@gmail.com>
2019-04-17 07:37:37 +0100

    Release 4.5.11

--- contract/src/main/java/uk/ac/manchester/owl/owlapi/PrepareForRelease.java
+++ contract/src/main/java/uk/ac/manchester/owl/owlapi/PrepareForRelease.java
@@ -11,8 +11,8 @@ import java.util.stream.Collectors;
 
 public class PrepareForRelease {
     public static void main(String[] args) throws IOException {
-        String newVersion = "4.5.10";
-        String[] toReplace = new String[] {"4.5.9", "4.5.9-SNAPSHOT"};
+        String newVersion = "4.5.11";
+        String[] toReplace = new String[] {"4.5.10", "4.5.10-SNAPSHOT"};
         treat(newVersion, toReplace,
             "../api/src/main/java/org/semanticweb/owlapi/util/VersionInfo.java");
         treat(newVersion, toReplace,
