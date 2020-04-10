15172ab8d3bd7b5c1f3eb7e07eb551696ae8178c
ignazio <ignazio.palmisano@gmail.com>
2020-04-10 20:44:36 +0100

    Fix OWLOntologyWriterConfiguration does not disable banner comments #904

--- api/src/main/java/org/semanticweb/owlapi6/model/parameters/ConfigurationOptions.java
+++ api/src/main/java/org/semanticweb/owlapi6/model/parameters/ConfigurationOptions.java
@@ -16,12 +16,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This enum handles default values and config file or system property
- * overrides. The config file name is {@code owlapi.properties}; to enable, make
- * sure the file is in the classpath. The property names are
- * {@code "org.semanticweb.owlapi.model.parameters.ConfigurationOptions."+name()}
- * (unchanged from previous OWLAPI versions). , both in the properties file and
- * in the system properties.
+ * This enum handles default values and config file or system property overrides. The config file
+ * name is {@code owlapi.properties}; to enable, make sure the file is in the classpath. The
+ * property names are {@code "org.semanticweb.owlapi.model.parameters.ConfigurationOptions."+name()}
+ * (unchanged from previous OWLAPI versions). , both in the properties file and in the system
+ * properties.
  */
 public enum ConfigurationOptions {
     //@formatter:off
@@ -83,7 +82,7 @@ public enum ConfigurationOptions {
     /** True if banners for ontology 
      * sections and entity comments 
      * should be outputted. */
-    BANNERS_ENABLED                     (Boolean.FALSE),
+    BANNERS_ENABLED                     (Boolean.TRUE),
     /** List of banned 
      * parsers keys. */
     BANNED_PARSERS                      (""),
@@ -129,7 +128,8 @@ public enum ConfigurationOptions {
     /**Max number of elements for caches.*/
     CACHE_SIZE                        (Integer.valueOf(2048));
     //@formatter:on
-    private static final String PREFIX = "org.semanticweb.owlapi.model.parameters.ConfigurationOptions.";
+    private static final String PREFIX =
+        "org.semanticweb.owlapi.model.parameters.ConfigurationOptions.";
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationOptions.class);
     private static final EnumMap<ConfigurationOptions, Object> owlapiProperties = loadProperties();
     private Object defaultValue;
@@ -141,7 +141,8 @@ public enum ConfigurationOptions {
     private static EnumMap<ConfigurationOptions, Object> loadProperties() {
         EnumMap<ConfigurationOptions, Object> map = new EnumMap<>(ConfigurationOptions.class);
         Properties props = new Properties();
-        try (InputStream stream = ConfigurationOptions.class.getResourceAsStream("/owlapi.properties")) {
+        try (InputStream stream =
+            ConfigurationOptions.class.getResourceAsStream("/owlapi.properties")) {
             if (stream != null) {
                 props.load(stream);
             }
@@ -158,9 +159,8 @@ public enum ConfigurationOptions {
     }
 
     /**
-     * @param parameterName
-     *        parameter name - by default the full name of this enumeration plus
-     *        the enum member name
+     * @param parameterName parameter name - by default the full name of this enumeration plus the
+     *                      enum member name
      * @return matching ConfigurationOptions member, or null if none found
      */
     @Nullable
@@ -172,12 +172,9 @@ public enum ConfigurationOptions {
     }
 
     /**
-     * @param value
-     *        value to parse according to the enum default value
-     * @param <T>
-     *        type for the returned value
-     * @param type
-     *        type of the returned value
+     * @param value value to parse according to the enum default value
+     * @param <T>   type for the returned value
+     * @param type  type of the returned value
      * @return parsed value
      */
     protected <T> T parse(Object value, Class<T> type) {
@@ -197,17 +194,13 @@ public enum ConfigurationOptions {
     }
 
     /**
-     * @param <T>
-     *        type for this value
-     * @param type
-     *        type for this value
-     * @param overrides
-     *        local overrides
-     * @return value for this configuration option. Values are evaluated as
-     *         follows: first, check overrides; if no overrides are present,
-     *         check if a system property with the expected name is set; if not,
-     *         check the config file; if no value is set in the config file, use
-     *         the default defined in this enumeration.
+     * @param <T>       type for this value
+     * @param type      type for this value
+     * @param overrides local overrides
+     * @return value for this configuration option. Values are evaluated as follows: first, check
+     *         overrides; if no overrides are present, check if a system property with the expected
+     *         name is set; if not, check the config file; if no value is set in the config file,
+     *         use the default defined in this enumeration.
      */
     public <T> T getValue(Class<T> type, @Nullable Map<ConfigurationOptions, Object> overrides) {
         Object override = overrides == null ? null : overrides.get(this);
@@ -227,10 +220,8 @@ public enum ConfigurationOptions {
     }
 
     /**
-     * @param <T>
-     *        type to cast to
-     * @param type
-     *        type to cast to
+     * @param <T>  type to cast to
+     * @param type type to cast to
      * @return default value
      */
     public <T> T getDefaultValue(Class<T> type) {
