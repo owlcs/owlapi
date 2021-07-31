fe4fa5f40a818a22ddb4df47b9f58abd69494797
ignazio <ignazio.palmisano@gmail.com>
2021-07-31 10:17:08 +0100

    security dependency updates

--- contract/src/main/java/uk/ac/manchester/owl/owlapi/PrepareForRelease.java
+++ contract/src/main/java/uk/ac/manchester/owl/owlapi/PrepareForRelease.java
@@ -11,9 +11,8 @@ import java.util.stream.Collectors;
 
 public class PrepareForRelease {
     public static void main(String[] args) throws IOException {
-        String newVersion = "4.5.20";
-        String[] toReplace =
-            new String[] {"4.5.18", "4.5.18-SNAPSHOT", "4.5.19-SNAPSHOT", "4.5.19"};
+        String newVersion = "4.5.20-SNAPSHOT";
+        String[] toReplace = new String[] {"4.5.20"};
         treat(newVersion, toReplace,
             "../api/src/main/java/org/semanticweb/owlapi/util/VersionInfo.java");
         treat(newVersion, toReplace,
