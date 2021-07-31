3012c740c59adf343ea90defaed3eff9c1c1754b
ignazio <ignazio.palmisano@gmail.com>
2021-07-31 20:51:11 +0100

    OWLAPI 5.1.19

--- api/src/main/java/org/semanticweb/owlapi/util/VersionInfo.java
+++ api/src/main/java/org/semanticweb/owlapi/util/VersionInfo.java
@@ -28,7 +28,7 @@ public class VersionInfo {
         if (v != null) {
             version = v;
         } else {
-            version = "5.1.19-SNAPSHOT";
+            version = "5.1.19";
         }
     }
 
--- contract/src/main/java/uk/ac/manchester/owl/owlapi/PrepareForRelease.java
+++ contract/src/main/java/uk/ac/manchester/owl/owlapi/PrepareForRelease.java
@@ -11,8 +11,8 @@ import java.util.stream.Collectors;
 
 public class PrepareForRelease {
     public static void main(String[] args) throws IOException {
-        String newVersion = "5.1.19-SNAPSHOT";
-        String[] toReplace = new String[] {"5.1.19"};
+        String newVersion = "5.1.19";
+        String[] toReplace = new String[] {"5.1.19-SNAPSHOT"};
         treat(newVersion, toReplace,
             "../api/src/main/java/org/semanticweb/owlapi/util/VersionInfo.java");
         treat(newVersion, toReplace,
