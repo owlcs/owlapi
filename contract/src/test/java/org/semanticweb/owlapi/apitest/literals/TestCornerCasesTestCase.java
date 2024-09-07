c6f2aa6ca8eef4cf70bbdcf3f69d3ebf031a20c5
ignazio <ignazio.palmisano@gmail.com>
2024-09-07 20:17:45 +0100

    Fix #1126 Handle exception when comparing integer literals.

--- contract/src/test/java/org/semanticweb/owlapi/apitest/literals/TestCornerCasesTestCase.java
+++ contract/src/test/java/org/semanticweb/owlapi/apitest/literals/TestCornerCasesTestCase.java
@@ -13,6 +13,7 @@
 package org.semanticweb.owlapi.apitest.literals;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -24,6 +25,7 @@ import org.semanticweb.owlapi.apitest.TestFiles;
 import org.semanticweb.owlapi.apitest.baseclasses.TestBase;
 import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat;
 import org.semanticweb.owlapi.formats.RDFXMLDocumentFormat;
+import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLOntology;
 
 class TestCornerCasesTestCase extends TestBase {
@@ -77,5 +79,13 @@ class TestCornerCasesTestCase extends TestBase {
         OWLOntology o = loadFrom(TestFiles.largeInteger, new FunctionalSyntaxDocumentFormat());
         assertTrue(saveOntology(o).toString().contains(INF));
         equal(o, roundTrip(o));
+    }
+
+
+    @Test
+    void testCompareWithLargeInteger() {
+        OWLLiteral smallInteger = df.getOWLLiteral(123);
+        OWLLiteral largeInteger = df.getOWLLiteral("149597870700", df.getIntegerOWLDatatype());
+        assertFalse(smallInteger.equals(largeInteger));
     }
 }
